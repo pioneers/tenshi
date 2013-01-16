5294f3c49f34e8f42a85f197a8f22cffa77df022
Robert Ou <rqou@berkeley.edu>
2013-01-15 16:27:21 -0800

    Fix reading multi-byte regs to actually work
    Add shadow registers so i2c master can properly read multi-byte registers
    
    Change-Id: I5e5a13ea5b8e96140f2eb5940d387d01f84525ab

--- grizzly_bear/firmware/firmware/i2c_register.c
+++ grizzly_bear/firmware/firmware/i2c_register.c
@@ -10,48 +10,56 @@ static unsigned char pwm_mode_new;
 
 // Autogenerate if statement needed for provide_i2c_reg.
 // offset is the REG_xxx define (i2c register address).
-// len is the REG_xxx_LEN define (size of the register).
-// val_addr is the address of the variable that should be set.
-#define AUTO_PROVIDE_REG(offset,len, var_addr)    \
-if (reg >= offset && reg < (offset + len))        \
-  return ((unsigned char *)var_addr)[reg-offset];
+// type is the REG_xxx_TYPE define (type of the register).
+// val_val is the value of the variable that should be sent. It may be a
+// function, in which case it will only be evaluated when needed.
+#define AUTO_PROVIDE_REG(offset, type, var_val)             \
+static type __shadow_##offset = 0;                          \
+if (reg >= offset && reg < (offset + sizeof(type))) {       \
+  if (reg == offset)                                        \
+    __shadow_##offset = var_val;                            \
+  return ((unsigned char *)&__shadow_##offset)[reg-offset]; \
+}
 
 // Autogenerate if statement needed for set_i2c_reg
 // offset is the REG_xxx define (i2c register address)
-// len is the REG_xxx_LEN define (size of the register)
+// type is the REG_xxx_TYPE define (type of the register).
 // val_addr is the address of the variable that should be set
-#define AUTO_SET_REG(offset,len, var_addr)    \
-if (reg >= offset && reg < (offset + len))        \
+#define AUTO_SET_REG(offset, type, var_addr)          \
+if (reg >= offset && reg < (offset + sizeof(type)))   \
   ((unsigned char *)var_addr)[reg-offset] = val;
 
 extern unsigned char provide_i2c_reg(unsigned char reg) {
   // special
   if (reg == REG_TYPE)
     return REG_TYPE_GRIZZLY_ID;
-  AUTO_PROVIDE_REG(REG_PWM_MODE, REG_PWM_MODE_LEN, &pwm_mode_new);
-  AUTO_PROVIDE_REG(REG_TARGET_SPEED_NEW, REG_TARGET_SPEED_NEW_LEN,
-      &target_speed_new);
-  AUTO_PROVIDE_REG(REG_CURRENT_PWM_MODE, REG_CURRENT_PWM_MODE_LEN, &pwm_mode);
-  AUTO_PROVIDE_REG(REG_CURRENT_TARGET_SPEED, REG_CURRENT_TARGET_SPEED_LEN,
-      get_target_speed_addr());
-  AUTO_PROVIDE_REG(REG_ISENSE_ADC, REG_ISENSE_ADC_LEN, get_isense_adc_addr());
-  AUTO_PROVIDE_REG(REG_ENCODER_COUNT, REG_ENCODER_COUNT_LEN,
-      get_encoder_count_addr());
-  AUTO_PROVIDE_REG(REG_REVISION, REG_REVISION_LEN, REVISION);
-  AUTO_PROVIDE_REG(REG_ILIMIT_ADC_THRESH, REG_ILIMIT_ADC_THRESH_LEN,
-      get_current_limit_adc_threshold_addr());
-  AUTO_PROVIDE_REG(REG_ILIMIT_CLAMP_PWM, REG_ILIMIT_CLAMP_PWM_LEN,
-      get_current_limit_clamp_pwm_addr());
-  AUTO_PROVIDE_REG(REG_ILIMIT_RETRY_TIME, REG_ILIMIT_RETRY_TIME_LEN,
-      get_current_limit_retry_time_addr());
-  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_NUM, REG_ILIMIT_RATIO_NUM_LEN,
-      get_current_limit_ratio_numerator_addr());
-  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_DENOM, REG_ILIMIT_RATIO_DENOM_LEN,
-      get_current_limit_ratio_denominator_addr());
-  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_MAX_USE, REG_ILIMIT_RATIO_MAX_USE_LEN,
-      get_current_limit_ratio_max_use_addr());
-  AUTO_PROVIDE_REG(REG_ACCEL_LIMIT, REG_ACCEL_LIMIT_LEN,
-      get_max_acceleration_addr());
+  AUTO_PROVIDE_REG(REG_PWM_MODE, REG_PWM_MODE_TYPE, pwm_mode_new);
+  AUTO_PROVIDE_REG(REG_TARGET_SPEED_NEW, REG_TARGET_SPEED_NEW_TYPE,
+      target_speed_new);
+  AUTO_PROVIDE_REG(REG_CURRENT_PWM_MODE, REG_CURRENT_PWM_MODE_TYPE, pwm_mode);
+  AUTO_PROVIDE_REG(REG_CURRENT_TARGET_SPEED, REG_CURRENT_TARGET_SPEED_TYPE,
+      get_target_speed_dangerous());
+  AUTO_PROVIDE_REG(REG_ISENSE_ADC, REG_ISENSE_ADC_TYPE,
+      get_isense_adc_dangerous());
+  AUTO_PROVIDE_REG(REG_ENCODER_COUNT, REG_ENCODER_COUNT_TYPE,
+      get_encoder_count_dangerous());
+  // special
+  if (reg >= REG_REVISION && reg < (REG_REVISION + REG_REVISION_LEN))
+    return ((unsigned char *)REVISION)[reg-REG_REVISION];
+  AUTO_PROVIDE_REG(REG_ILIMIT_ADC_THRESH, REG_ILIMIT_ADC_THRESH_TYPE,
+      get_current_limit_adc_threshold_dangerous());
+  AUTO_PROVIDE_REG(REG_ILIMIT_CLAMP_PWM, REG_ILIMIT_CLAMP_PWM_TYPE,
+      get_current_limit_clamp_pwm_dangerous());
+  AUTO_PROVIDE_REG(REG_ILIMIT_RETRY_TIME, REG_ILIMIT_RETRY_TIME_TYPE,
+      get_current_limit_retry_time_dangerous());
+  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_NUM, REG_ILIMIT_RATIO_NUM_TYPE,
+      get_current_limit_ratio_numerator_dangerous());
+  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_DENOM, REG_ILIMIT_RATIO_DENOM_TYPE,
+      get_current_limit_ratio_denominator_dangerous());
+  AUTO_PROVIDE_REG(REG_ILIMIT_RATIO_MAX_USE, REG_ILIMIT_RATIO_MAX_USE_TYPE,
+      get_current_limit_ratio_max_use_dangerous());
+  AUTO_PROVIDE_REG(REG_ACCEL_LIMIT, REG_ACCEL_LIMIT_TYPE,
+      get_max_acceleration_dangerous());
 
   return UNUSED_REG_VAL;
 }
@@ -62,21 +70,21 @@ extern void set_i2c_reg(unsigned char reg, unsigned char val) {
     pwm_mode = pwm_mode_new;
     set_target_speed_dangerous(target_speed_new);
   }
-  AUTO_SET_REG(REG_PWM_MODE, REG_PWM_MODE_LEN, &pwm_mode_new);
-  AUTO_SET_REG(REG_TARGET_SPEED_NEW, REG_TARGET_SPEED_NEW_LEN,
+  AUTO_SET_REG(REG_PWM_MODE, REG_PWM_MODE_TYPE, &pwm_mode_new);
+  AUTO_SET_REG(REG_TARGET_SPEED_NEW, REG_TARGET_SPEED_NEW_TYPE,
       &target_speed_new);
-  AUTO_SET_REG(REG_ILIMIT_ADC_THRESH, REG_ILIMIT_ADC_THRESH_LEN,
+  AUTO_SET_REG(REG_ILIMIT_ADC_THRESH, REG_ILIMIT_ADC_THRESH_TYPE,
       get_current_limit_adc_threshold_addr());
-  AUTO_SET_REG(REG_ILIMIT_CLAMP_PWM, REG_ILIMIT_CLAMP_PWM_LEN,
+  AUTO_SET_REG(REG_ILIMIT_CLAMP_PWM, REG_ILIMIT_CLAMP_PWM_TYPE,
       get_current_limit_clamp_pwm_addr());
-  AUTO_SET_REG(REG_ILIMIT_RETRY_TIME, REG_ILIMIT_RETRY_TIME_LEN,
+  AUTO_SET_REG(REG_ILIMIT_RETRY_TIME, REG_ILIMIT_RETRY_TIME_TYPE,
       get_current_limit_retry_time_addr());
-  AUTO_SET_REG(REG_ILIMIT_RATIO_NUM, REG_ILIMIT_RATIO_NUM_LEN,
+  AUTO_SET_REG(REG_ILIMIT_RATIO_NUM, REG_ILIMIT_RATIO_NUM_TYPE,
       get_current_limit_ratio_numerator_addr());
-  AUTO_SET_REG(REG_ILIMIT_RATIO_DENOM, REG_ILIMIT_RATIO_DENOM_LEN,
+  AUTO_SET_REG(REG_ILIMIT_RATIO_DENOM, REG_ILIMIT_RATIO_DENOM_TYPE,
       get_current_limit_ratio_denominator_addr());
-  AUTO_SET_REG(REG_ILIMIT_RATIO_MAX_USE, REG_ILIMIT_RATIO_MAX_USE_LEN,
+  AUTO_SET_REG(REG_ILIMIT_RATIO_MAX_USE, REG_ILIMIT_RATIO_MAX_USE_TYPE,
       get_current_limit_ratio_max_use_addr());
-  AUTO_SET_REG(REG_ACCEL_LIMIT, REG_ACCEL_LIMIT_LEN,
+  AUTO_SET_REG(REG_ACCEL_LIMIT, REG_ACCEL_LIMIT_TYPE,
       get_max_acceleration_addr());
 }
--- grizzly_bear/firmware/includes/i2c_register.h
+++ grizzly_bear/firmware/includes/i2c_register.h
@@ -2,6 +2,8 @@
 #define I2C_REGISTER_H_
 // This file contains code for the I2C registers.
 
+#include <stdint.h>
+
 // Gives the value for a particular I2C register.
 // Register is passed as an argument and this function returns a byte.
 // WARNING: This function is called from an ISR. It must complete reasonably
@@ -21,35 +23,35 @@ extern void set_i2c_reg(unsigned char reg, unsigned char val);
 // register, so it would probably return 00.
 #define REG_TYPE_GRIZZLY_ID             'G'
 #define REG_PWM_MODE                  0x01
-#define REG_PWM_MODE_LEN                1
+#define REG_PWM_MODE_TYPE             uint8_t
 #define REG_TARGET_SPEED_NEW          0x04
-#define REG_TARGET_SPEED_NEW_LEN        4
+#define REG_TARGET_SPEED_NEW_TYPE     uint32_t
 // special
 #define REG_APPLY_NEW_SPEED           0x08
 #define REG_CURRENT_PWM_MODE          0x09
-#define REG_CURRENT_PWM_MODE_LEN        1
+#define REG_CURRENT_PWM_MODE_TYPE     uint8_t
 #define REG_CURRENT_TARGET_SPEED      0x0C
-#define REG_CURRENT_TARGET_SPEED_LEN    4
+#define REG_CURRENT_TARGET_SPEED_TYPE uint32_t
 #define REG_ISENSE_ADC                0x10
-#define REG_ISENSE_ADC_LEN              2
+#define REG_ISENSE_ADC_TYPE           uint16_t
 #define REG_ENCODER_COUNT             0x20
-#define REG_ENCODER_COUNT_LEN           4
+#define REG_ENCODER_COUNT_TYPE        int32_t
 #define REG_REVISION                  0x60
 #define REG_REVISION_LEN                21
 #define REG_ILIMIT_ADC_THRESH         0x82
-#define REG_ILIMIT_ADC_THRESH_LEN       2
+#define REG_ILIMIT_ADC_THRESH_TYPE    uint16_t
 #define REG_ILIMIT_CLAMP_PWM          0x84
-#define REG_ILIMIT_CLAMP_PWM_LEN        2
+#define REG_ILIMIT_CLAMP_PWM_TYPE     int16_t
 #define REG_ILIMIT_RETRY_TIME         0x86
-#define REG_ILIMIT_RETRY_TIME_LEN       2
+#define REG_ILIMIT_RETRY_TIME_TYPE    uint16_t
 #define REG_ILIMIT_RATIO_NUM          0x88
-#define REG_ILIMIT_RATIO_NUM_LEN        1
+#define REG_ILIMIT_RATIO_NUM_TYPE     uint8_t
 #define REG_ILIMIT_RATIO_DENOM        0x89
-#define REG_ILIMIT_RATIO_DENOM_LEN      1
+#define REG_ILIMIT_RATIO_DENOM_TYPE   uint8_t
 #define REG_ILIMIT_RATIO_MAX_USE      0x8A
-#define REG_ILIMIT_RATIO_MAX_USE_LEN    2
+#define REG_ILIMIT_RATIO_MAX_USE_TYPE uint16_t
 #define REG_ACCEL_LIMIT               0x90
-#define REG_ACCEL_LIMIT_LEN             2
+#define REG_ACCEL_LIMIT_TYPE          int16_t
 
 // This value is returned when reading a register that is not defined.
 #define UNUSED_REG_VAL  0xFF
