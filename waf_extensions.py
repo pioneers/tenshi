bb6858007478b262b328e91774b7f3dd6694b3da
Robert Ou <robertou@pioneers.berkeley.edu>
2013-08-23 13:41:29 -0700

    Set up build variants
    Change-Id: Iab1989e5e65c4d93555b3679b9e2136d995afde3

---
+++ waf_extensions.py
@@ -0,0 +1,11 @@
+from waflib.Build import BuildContext, CleanContext, \
+    InstallContext, UninstallContext
+
+def declare_variants(*args):
+    for x in args:
+        for y in (BuildContext, CleanContext, InstallContext, UninstallContext):
+            name = y.__name__.replace('Context','').lower()
+            # Examples of name: build, clean, install...
+            class tmp(y):
+                cmd = name + '_' + x
+                variant = x
