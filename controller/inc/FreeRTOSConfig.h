f12722c15296ea2f6a03678ce076cb8cb7e420ca
Robert Ou <robertou@pioneers.berkeley.edu>
2013-09-02 16:10:09 -0700

    Wrote an actually-working FreeRTOS LED blinker
    Change-Id: Ibd378dbc02fc6a4c55e765a004a41560defae316
    Reviewed-on: https://robertou.com/gerrit/58
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- controller/inc/FreeRTOSConfig.h
+++ controller/inc/FreeRTOSConfig.h
@@ -85,7 +85,7 @@
 #endif
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				1
+#define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( 168000000 )
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
@@ -100,7 +100,7 @@
 #define configQUEUE_REGISTRY_SIZE		8
 #define configCHECK_FOR_STACK_OVERFLOW	0
 #define configUSE_RECURSIVE_MUTEXES		1
-#define configUSE_MALLOC_FAILED_HOOK	1
+#define configUSE_MALLOC_FAILED_HOOK	0
 #define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	1
 #define configGENERATE_RUN_TIME_STATS	0
