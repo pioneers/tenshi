eb84a074ff9011ac961cb335a79b3e54d93d8406
Robert Ou <robertou@pioneers.berkeley.edu>
2013-09-01 19:48:08 -0700

    Move embedded code into its own subdirectory
    Change-Id: I4677155d3b26a1e4d48348ba9599391a5571007a
    Reviewed-on: https://robertou.com/gerrit/53
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>
    Tested-by: Jenkins CI

---
+++ controller/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+# Set up tools (assumes jenkins copied them or manually copied into tools directory)
+./tools/extract-tools.sh
+
+# TODO(rqou): Less hacky
+export PATH=$PATH:`pwd`/tools/arm-toolchain/bin
+
+# Main compile process
+./waf configure build
+
+# Run cpplint
+./tools/run-cpplint.py
