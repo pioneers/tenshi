d3dc8cc9fb8b5bdfd2125055b56d043ab8c3a85f
Robert Ou <robertou@pioneers.berkeley.edu>
2013-09-16 19:30:10 -0700

    Work around XULRunner issue on Mac OS X
    Change-Id: I8c59bde19aa10f373883ea6774fa42654a99eec5
    Reviewed-on: https://robertou.com/gerrit/64
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/build.sh
+++ angel-player/build.sh
@@ -12,12 +12,10 @@ if [ ! -e xulrunner-22.0.en-US.win32.zip ]
 then
     wget ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/22.0/runtimes/xulrunner-22.0.en-US.win32.zip
 fi
-# Mac OS X version is disabled right now because XULRunner seems to be broken
-# https://bugzilla.mozilla.org/show_bug.cgi?id=738790
-# if [ ! -e xulrunner-22.0.en-US.mac.tar.bz2 ]
-# then
-#     wget ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/22.0/runtimes/xulrunner-22.0.en-US.mac.tar.bz2
-# fi
+if [ ! -e xulrunner-22.0.en-US.mac.tar.bz2 ]
+then
+    wget ftp://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/22.0/runtimes/xulrunner-22.0.en-US.mac.tar.bz2
+fi
 
 # Prepare linux version
 rm -rf angel-player-linux-x86_64
@@ -38,26 +36,25 @@ cp -r ../../src/* .
 cd ..
 
 # Prepare mac version
-# rm -rf angel-player-mac.app
-# mkdir angel-player-mac.app
-# cd angel-player-mac.app
-# # Mac OS requires a bunch of random futzing with stuff
-# mkdir -p Contents/Frameworks
-# mkdir -p Contents/Resources
-# mkdir -p Contents/MacOS
-# pushd Contents/Frameworks
-# tar xjf ../../../xulrunner-22.0.en-US.mac.tar.bz2
-# popd
-# cp Contents/Frameworks/XUL.framework/Versions/Current/xulrunner Contents/MacOS/xulrunner
-# cp -r ../../src/* Contents/Resources
-# cp -r ../../meta-mac/* Contents
-# cd ..
+rm -rf angel-player-mac.app
+mkdir angel-player-mac.app
+cd angel-player-mac.app
+# Mac OS requires a bunch of random futzing with stuff
+mkdir -p Contents/Frameworks
+mkdir -p Contents/Resources
+mkdir -p Contents/MacOS
+pushd Contents/Frameworks
+tar xjf ../../../xulrunner-22.0.en-US.mac.tar.bz2
+popd
+cp -r ../../src/* Contents/Resources
+cp -r ../../meta-mac/* Contents
+cd ..
 
-../inject-version.py ../src/application.ini angel-player-linux-x86_64/application.ini angel-player-win32/application.ini
+../inject-version.py ../src/application.ini angel-player-linux-x86_64/application.ini angel-player-win32/application.ini angel-player-mac.app/Contents/Resources/application.ini
 
 # Archive the outputs
 tar cjf angel-player-linux-x86_64.tar.bz2 angel-player-linux-x86_64
 tar cjf angel-player-win32.tar.bz2 angel-player-win32
-# tar cjf angel-player-mac.tar.bz2 angel-player-mac.app
+tar cjf angel-player-mac.tar.bz2 angel-player-mac.app
 
 cd ..
