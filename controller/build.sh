b8cc0268b8d217709b29a6c08dcec7cd39888369
Robert Ou <robertou@pioneers.berkeley.edu>
2013-09-21 01:39:43 -0700

    Testing violations
    Change-Id: Ib38550856f25cdfce80a5bf869f103ec63089c68

--- controller/build.sh
+++ controller/build.sh
@@ -10,4 +10,4 @@ export PATH=$PATH:`pwd`/tools/arm-toolchain/bin
 ./waf configure build
 
 # Run cpplint
-./tools/run-cpplint.py
+./tools/run-cpplint.py 2>&1 | tee build/cpplint.txt ; ( exit ${PIPESTATUS[0]} )
