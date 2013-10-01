d659b94679b6e3fdf84b5f87fd2231b2177a31ff
Robert Ou <robertou@pioneers.berkeley.edu>
2013-09-30 21:56:03 -0700

    Enable pep8 on python files
    Also fix pep8 errors
    
    Change-Id: I0073bfbbd5fc0a471d477d4701f19a70e49abbdf
    Reviewed-on: https://robertou.com/gerrit/69
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- tools/version_info_helpers.py
+++ tools/version_info_helpers.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import time
 
+
 # Returns the git hash of the current head. If c_array_mode is false, returns
 # a string with just the hash. If c_array_mode is true, returns a string broken
 # up into bytes and with 0x prefixed to each byte.
@@ -19,11 +20,14 @@ def get_git_hash(c_array_mode):
         rev += "0x" + head_hash[i*2:i*2+2] + ", "
     return rev
 
+
 # Returns True iff the working directory is considered clean (not including
 # untracked files).
 def get_working_dir_clean():
-    git_status = subprocess.check_output(['git', 'status', '--porcelain', '-uno']).strip()
+    git_status = subprocess.check_output(
+        ['git', 'status', '--porcelain', '-uno']).strip()
     return len(git_status) == 0
+
 
 # Returns True iff this is a Jenkins CI build that ends with "-committed"
 def get_building_from_jenkins():
@@ -32,6 +36,7 @@ def get_building_from_jenkins():
     if not os.environ['JOB_NAME'].endswith("-committed"):
         return False
     return True
+
 
 # Return the current time formatted into a string
 def get_build_time():
