0de51cc5cc1b4c12a3d97f7ee6a3049a4a47cdf9
Kyle Zentner <zentner.kyle@gmail.com>
2013-10-24 00:32:46 -0700

    Added xregexp submodule for Angelic.
    Change-Id: I25af63a7b12bd2dd855f00d29f618735e3cfbbd3

---
+++ angel-player/src/chrome/content/angelic/build.sh
@@ -0,0 +1,7 @@
+XREGEXP_PATH=angel-player/src/chrome/content/angelic/xregexp
+cd $PROJECT_ROOT_DIR
+git submodule update --init $XREGEXP_PATH
+cd $PROJECT_ROOT_DIR
+cd $XREGEXP_PATH
+git reset --hard
+git apply ../xregexp.patch
