cb338502389c6d1ff86cd825090f56d7e029af59
Kyle Zentner <zentner.kyle@gmail.com>
2013-10-23 22:50:51 -0700

    Added string_map implementation for Angelic.
    Change-Id: I32f2b922c7406e7fcfa5eedbf5ecff8281e58cc7

---
+++ angel-player/src/chrome/content/angelic/string_map.js
@@ -0,0 +1,95 @@
+var require = require;
+var export_vals;
+
+if ( require === undefined ) {
+  export_vals = function ( names, vals ) {
+    };
+  require = function ( filename ) {
+    return Components.utils.import ( 'chrome://angel-player/content/angelic/' + filename );
+    };
+  }
+else {
+  export_vals = function ( names, vals ) {
+    var n;
+
+    for ( n in names ) {
+      exports[names[n]] = vals[n];
+      }
+    };
+  }
+
+//
+// This module provides a simple string -> obj map.
+//
+
+var make = function make ( obj ) {
+  var storage = {};
+
+  if ( obj !== undefined ) {
+    for ( var k in obj ) {
+      storage[k + '$'] = obj[k];
+      }
+    }
+  return {
+    // Get the value, or set it to alternative.
+    'get': function ( key, alternative ) {
+      var val = storage[key + '$'];
+      if ( val === undefined ) {
+        this.set ( key, alternative );
+        }
+      return storage[key + '$'];
+      },
+    'set': function ( key, val ) {
+      storage[key + '$'] = val;
+      return this;
+      },
+    'has': function ( key ) {
+      return key + '$' in storage;
+      },
+    'delete': function ( key ) {
+      return delete storage[key + '$'];
+      },
+    // Update values in place using a function.
+    'map': function ( func ) {
+      var self = this;
+
+      this.each ( function ( key, val ) {
+        self.set ( key, func ( key, val ) );
+        } );
+      return this;
+      },
+    // Run a function on every key, value pair.
+    'each': function ( func ) {
+      var key;
+      var val;
+
+      for ( key in storage ) {
+        val = storage[key];
+        key = key.substr( 0, key.length - 1 );
+        func ( key, val );
+        }
+      return this;
+      },
+    'toString': function ( val_str ) {
+      var out = '{';
+
+
+      if ( val_str === undefined ) {
+        val_str = function ( val ) {
+          return val.toString ( );
+          };
+        }
+      this.each ( function ( key, val ) {
+        out += '' + key + ' : ' + val_str ( val ) + ',';
+        } );
+
+      out += '}';
+      return out;
+      },
+    };
+  };
+
+var EXPORTED_SYMBOLS = ['make'];
+var exported_objects = [ make ];
+export_vals ( EXPORTED_SYMBOLS,
+              exported_objects );
