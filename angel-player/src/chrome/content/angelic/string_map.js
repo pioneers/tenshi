8dbd92455e951ec1c43155b9c792b6dbe5336ad2
Kyle Zentner <zentner.kyle@gmail.com>
2013-10-23 22:50:51 -0700

    Added string_map implementation for Angelic.
    Change-Id: I32f2b922c7406e7fcfa5eedbf5ecff8281e58cc7
    Reviewed-on: https://robertou.com/gerrit/83
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

---
+++ angel-player/src/chrome/content/angelic/string_map.js
@@ -0,0 +1,101 @@
+// Import Machinery used to make xulrunner and node.js act the same.
+var require = require;
+var export_vals;
+
+if ( require === undefined ) {
+  export_vals = function ( names, vals ) {
+    };
+  require = function ( filename ) {
+    var module = {};
+    Components.utils.import ( 'chrome://angel-player/content/angelic/' + filename, module );
+    return module;
+    };
+  }
+else {
+  export_vals = function ( names, vals ) {
+    var n;
+
+    for ( n in names ) {
+      exports[names[n]] = vals[n];
+      }
+    };
+  }
+// End Import Machinery
+
+//
+// This module provides a simple string -> obj map.
+//
+
+var make = function make ( obj ) {
+  var storage = {};
+
+  if ( obj !== undefined ) {
+    for ( var k in obj ) {
+      storage[k + '$'] = obj[k];
+      }
+    }
+  return {
+    // Get the value, or set it to alternative.
+    'get': function ( key, alternative ) {
+      var val = storage[key + '$'];
+      if ( val === undefined ) {
+        this.set ( key, alternative );
+        }
+      return storage[key + '$'];
+      },
+    'set': function ( key, val ) {
+      storage[key + '$'] = val;
+      return this;
+      },
+    'has': function ( key ) {
+      return key + '$' in storage;
+      },
+    'delete': function ( key ) {
+      return delete storage[key + '$'];
+      },
+    // Update values in place using a function.
+    'map': function ( func ) {
+      var self = this;
+
+      this.each ( function ( key, val ) {
+        self.set ( key, func ( key, val ) );
+        } );
+      return this;
+      },
+    // Run a function on every key, value pair.
+    'each': function ( func ) {
+      var key;
+      var val;
+
+      for ( key in storage ) {
+        val = storage[key];
+        key = key.substr( 0, key.length - 1 );
+        func ( key, val );
+        }
+      return this;
+      },
+    'toString': function ( val_str ) {
+      var out = '{';
+
+
+      if ( val_str === undefined ) {
+        val_str = function ( val ) {
+          return val.toString ( );
+          };
+        }
+      this.each ( function ( key, val ) {
+        out += '' + key + ' : ' + val_str ( val ) + ',';
+        } );
+
+      out += '}';
+      return out;
+      },
+    };
+  };
+
+// Export Machinery to make node.js and xulrunner act the same.
+var EXPORTED_SYMBOLS = ['make'];
+var exported_objects = [ make ];
+export_vals ( EXPORTED_SYMBOLS,
+              exported_objects );
+// End Export Machinery
