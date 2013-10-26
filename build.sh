8a0ac662b3a97f6d4b0413a3bf291745fd2ef8c4
Kyle Zentner <zentner.kyle@gmail.com>
2013-10-26 14:48:23 -0700

    Fixed build.sh to not exit after first linter failure.
    Change-Id: I608cb388d34f6cb884b0fc010abf7d934476b49e

--- build.sh
+++ build.sh
@@ -16,11 +16,14 @@ do
 	./$dir/build.sh
 done
 
+STATUS=`true`
 # Run cpplint
-./tools/run-style-tool.py cpplint 2>&1 | tee build/cpplint.txt ; ( exit ${PIPESTATUS[0]} )
+./tools/run-style-tool.py cpplint 2>&1 | tee build/cpplint.txt || STATUS=`false`
 # Run pep8
-./tools/run-style-tool.py pep8 2>&1 | tee build/pep8.txt ; ( exit ${PIPESTATUS[0]} )
+./tools/run-style-tool.py pep8 2>&1 | tee build/pep8.txt || STATUS=`false`
 # Run jshint
-./tools/run-style-tool.py jshint 2>&1 | tee build/jshint.txt ; ( exit ${PIPESTATUS[0]} )
+./tools/run-style-tool.py jshint 2>&1 | tee build/jshint.txt || STATUS=`false`
 # Run csslint
-./tools/run-style-tool.py csslint 2>&1 | tee build/csslint.txt ; ( exit ${PIPESTATUS[0]} )
+./tools/run-style-tool.py csslint 2>&1 | tee build/csslint.txt || STATUS=`false`
+
+exit $STATUS
