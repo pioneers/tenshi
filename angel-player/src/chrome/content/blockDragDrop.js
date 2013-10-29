3c3f2fa98d8d933294c0281a16ee77cf7c3eb52a
Robert Ou <robertou@pioneers.berkeley.edu>
2013-10-28 18:46:03 -0700

    Add primitive block drag logic
    NOTE: This currently DOES NOT update the sorted block list.
    This will be fixed in the next commit.
    
    Change-Id: I7ef4e2c5202d8317ae33f8f08a24ca643a15972a

--- angel-player/src/chrome/content/blockDragDrop.js
+++ angel-player/src/chrome/content/blockDragDrop.js
@@ -4,13 +4,14 @@ var blockDnD = {};
 
 var MOUSE_MODE_NONE = 0;
 var MOUSE_MODE_PANNING = 1;
+var MOUSE_MODE_DRAG_BLOCK = 2;
 
 var document = null;
 var mainSvg = null;
 var mainProgram = null;
 var currentMouseMode = 0;
 
-// Current SVG panning offset.
+// Current SVG panning offset. This is in pixels.
 var svgPanX = 0;
 var svgPanY = 0;
 
@@ -19,6 +20,7 @@ var svgPanY = 0;
 // the main canvas.
 var curDragX = 0;
 var curDragY = 0;
+var curDragElement = null;
 
 function dragMouseDown(evt) {
     // dump(evt);
@@ -26,6 +28,36 @@ function dragMouseDown(evt) {
         // Don't handle a new action if we're already doing one.
 
         switch (evt.button) {
+            case 0:
+                // We may at some point want to terminate some/all in-progress
+                // interactions at this point.
+
+                // Walk up the DOM until we either find either a code block
+                // element or the main SVG
+                var targetElem = evt.target;
+                while (targetElem.getAttributeNS(null, 'class') !== 'codeBlock'
+                    && targetElem !== mainSvg) {
+                    targetElem = targetElem.parentNode;
+                }
+                // TODO(rqou): Can we ever end up with other random node?
+
+                if (targetElem != mainSvg) {
+                    // TODO(rqou): Sigh, figure out this px/em bullshit later.
+                    // This is currently in px and being coverted to em
+                    var clickX = (evt.clientX - svgPanX) / 16.0;
+                    var clickY = (evt.clientY - svgPanY) / 16.0;
+
+                    // Where we clicked in the block
+                    curDragX = clickX - targetElem.blockData.x;
+                    curDragY = clickY - targetElem.blockData.y;
+
+                    curDragElement = targetElem;
+                    // TODO(rqou): Do we need to copypasta this?
+                    targetElem.setAttributeNS(null, 'pointer-events', 'none');
+                    currentMouseMode = MOUSE_MODE_DRAG_BLOCK;
+                }
+
+                break;
             case 1:
                 // Middle mouse button, start panning
                 currentMouseMode = MOUSE_MODE_PANNING;
@@ -43,6 +75,14 @@ function dragMouseDown(evt) {
 
 function dragMouseUp(evt) {
     switch (evt.button) {
+        case 0:
+            if (currentMouseMode == MOUSE_MODE_DRAG_BLOCK) {
+                // Release left button, stop dragging
+                currentMouseMode = MOUSE_MODE_NONE;
+                curDragElement.setAttributeNS(null, 'pointer-events', 'auto');
+                curDragElement = null;
+            }
+            break;
         case 1:
             if (currentMouseMode == MOUSE_MODE_PANNING) {
                 // Release middle button, stop panning
@@ -67,6 +107,27 @@ function dragMouseMove(evt) {
             var transform = mainSvg.createSVGTransform();
             transform.setTranslate(svgPanX, svgPanY);
             mainSvg.transform.baseVal.initialize(transform);
+            break;
+        case MOUSE_MODE_DRAG_BLOCK:
+            // Dragging a block
+
+            // Figure out where we are now
+            var clickX = (evt.clientX - svgPanX) / 16.0;
+            var clickY = (evt.clientY - svgPanY) / 16.0;
+
+            // New location
+            var newX = clickX - curDragX;
+            var newY = clickY - curDragY;
+
+            curDragElement.blockData.x = newX;
+            curDragElement.blockData.y = newY;
+
+            var transform = mainSvg.createSVGTransform();
+            transform.setTranslate(newX, newY);
+            curDragElement.transform.baseVal.initialize(transform);
+
+            // TODO(rqou): Update all the relevant state for element position.
+            // TODO(rqou): Do snapping logic.
             break;
         default:
             break;
