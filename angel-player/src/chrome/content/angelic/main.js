e827eb0d2d4f62e6302cfd9e7098504ec9c38820
Kyle Zentner <zentner.kyle@gmail.com>
2013-10-30 20:56:44 -0700

    Added rest of VM for Angelic.
    Change-Id: I4c4e875fb36d80a6d659b8c5a83d734493a2fb45

--- angel-player/src/chrome/content/angelic/main.js
+++ angel-player/src/chrome/content/angelic/main.js
@@ -26,6 +26,8 @@ var scope = require ( './scope.js' );
 var string_map = require ( './string_map.js' );
 var parser = require ( './parser.js' );
 var misc = require ( './misc.js' );
+var compiler = require ( './compiler.js' );
+var executor = require ( './executor.js' );
 
 //
 // This is the main Angelic module
@@ -35,33 +37,47 @@ var misc = require ( './misc.js' );
 function compile_and_run ( text ) {
   var scopes = string_map.make ( );
   var a_parser = parser.make ( );
+  var a_compiler = compiler.make ( );
+  var a_executor = executor.make ( );
   var parse_tree;
+  var lib;
 
   a_parser.setupScopes ( scopes );
+  a_compiler.setupScopes ( scopes );
 
   parse_tree = a_parser.parse ( text );
 
   misc.print ( parse_tree );
 
-  // More functionality added here later.
+  lib = a_compiler.compile ( parse_tree );
+
+  a_executor.execute ( lib );
   }
 
 var to_parse = '' +
 'fn fib n:\n' +
-'    a = 1\n' +
+'    n = n - 1\n' +
+'    a = 0\n' +
 '    b = 1\n' +
 '    while n != 0:\n' +
 '        temp = a + b\n' +
 '        a = b\n' +
 '        b = temp\n' +
 '        n = n - 1\n' +
-'    return n\n' +
+'    print (n)\n' +
 'fn main:\n' +
-'    fib ( 100 )\n' +
+'    n = 50 - 1\n' +
+'    a = 0\n' +
+'    b = 1\n' +
+'    while n != 0:\n' +
+'        temp = a + b\n' +
+'        a = b\n' +
+'        b = temp\n' +
+'        n = n - 1\n' +
+'    print (b)\n' +
 'fn test ():\n' +
-'    test = 5\n' +
 '    if 0: test = 1\n' +
-'    else: text = 2\n';
+'    test = 5\n';
 
 compile_and_run ( to_parse );
 
