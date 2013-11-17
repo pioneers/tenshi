0e118219c9fa516828c47b233b222f80b7b65a18
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2013-11-17 15:56:25 -0800

    Added motors and sensors
    Change-Id: I2b0e56f491af40bacc415673f88651fc402634c4

---
+++ angel-player/src/chrome/content/simulator/Sensors/Rangefinder.js
@@ -0,0 +1,45 @@
+/*
+All rangefinders are a single box : it shoots a ray out at its <0, 0, 1> direction
+    run shoots a ray and sets its value to the distance
+*/
+function Rangefinder(width, length, height, mass, iniX, iniY, iniZ)
+{
+    this.rangefinder = createBox(width, length, height, mass, 0x000000, iniX, iniY, iniZ);
+    this.physicsObject = this.rangefinder;
+}
+
+Rangefinder.prototype.getVal = function()
+{
+    return this.value;
+};
+
+Rangefinder.prototype.run = function()
+{
+    var tr = new Ammo.btTransform();
+    this.rangefinder.getMotionState().getWorldTransform(tr);
+
+    var pos = tr.getOrigin();
+    var rot = tr.getRotation();
+    var upQ = new Ammo.btQuaternion(0, 0, 1, 0);
+
+    var nPos = rotateV3ByQuat(upQ, rot);
+
+    var distance = function(a, b)
+    {
+        return Math.sqrt((a.x() - b.x())*(a.x() - b.x()) +
+                        (a.y() - b.y())*(a.y() - b.y()) +
+                        (a.z() - b.z())*(a.z() - b.z()));
+    };
+
+    nPos = new Ammo.btVector3(pos.x() + 1000*nPos.x(), pos.y() + 1000*nPos.y(), pos.z() + 1000*nPos.z());
+
+    this.raycast = new Ammo.ClosestRayResultCallback(pos, nPos);
+
+    scene.world.rayTest(pos, nPos, this.raycast);
+    if(this.raycast.hasHit)
+    {
+        var end = this.raycast.get_m_hitPointWorld();
+
+        this.value = distance(pos, end);
+    }
+};
