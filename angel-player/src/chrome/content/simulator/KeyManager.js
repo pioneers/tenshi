7bdba9ab1c07ea56c38442f2091b1099ef554015
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2013-11-17 16:30:57 -0800

    Added various other functions
    Change-Id: I9dfde1494e2dfc0f5e9f4b7a78e24b3c7547baa8
    Reviewed-on: https://robertou.com/gerrit/143
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

---
+++ angel-player/src/chrome/content/simulator/KeyManager.js
@@ -0,0 +1,63 @@
+DOWN = 0;
+HOLD = 1;
+UP = 2;
+
+function KeyManager()
+{
+    this.state = {};
+    this.funcs = [{}, {}, {}];
+    document.onkeydown = this.updateDown;
+    document.onkeyup = this.updateUp;
+}
+
+KeyManager.prototype.updateDown = function(e)
+{
+    var key = e.which || e.keyCode; //some browsers use different
+
+    this.state[key] = 1;
+
+    this.actOnFuncs(DOWN, key);
+};
+
+KeyManager.prototype.updateUp = function(e)
+{
+    var key = e.which || e.keyCode; //some browsers use different
+
+    this.state[key] = 0;
+
+    this.actOnFuncs(UP, key);
+};
+
+KeyManager.prototype.updateHold = function()
+{
+    for(var key in this.state)
+    {
+        if(this.state[key])
+        {
+            this.actOnFuncs(HOLD, key);
+        }
+    }
+};
+
+KeyManager.prototype.bindFunc = function(type, key, func)
+{
+    if(this.funcs[type][key])
+    {
+        this.funcs[type][key].push(func);
+    }
+    else
+    {
+        this.funcs[type][key] = [func];
+    }
+};
+
+KeyManager.prototype.actOnFuncs = function(type, key)
+{
+    if(this.func[type][key])
+    {
+        for(var i = 0; i < this.func[type][key].length; i++)
+        {
+            this.func[type][key][i]();
+        }
+    }
+};
