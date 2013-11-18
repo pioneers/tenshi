029103a01d3f51d2218108e7ee316bc769c70fa6
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2013-11-17 16:00:53 -0800

    Added some classes
    Change-Id: I0e4ffe5a6352d9ce19e1f264d5d6068614d8634d

---
+++ angel-player/src/chrome/content/simulator/MasterObject.js
@@ -0,0 +1,30 @@
+function MasterObject()
+{
+    this.motors = {};
+    this.sensors = {};
+    this.version = 0;
+    this.isPaused = 0;
+}
+
+MasterObject.prototype.saveMotors = function()
+{
+    var motors = {};
+    motors.version = this.version;
+
+    for(motor in this.motors)
+    {
+        motors[motor] = this.motors[motor];
+    }
+
+    return motors;
+}
+
+MasterObject.prototype.loadMotors = function(state)
+{
+    for(motor in state)
+    {
+        this.motors[motor] = state[motor];
+    }
+
+    this.version = state.version;
+}
