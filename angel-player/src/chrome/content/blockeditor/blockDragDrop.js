2ced55a55afe79da997fd445956755b186e32358
Robert Ou <robertou@pioneers.berkeley.edu>
2013-12-07 01:07:07 -0800

    stupid dump function
    Change-Id: I01082162b88ce4cc7c322094f63914a62b50189b

--- angel-player/src/chrome/content/blockeditor/blockDragDrop.js
+++ angel-player/src/chrome/content/blockeditor/blockDragDrop.js
@@ -43,7 +43,18 @@ function recursivelyReparentNodes(startNode, newParent) {
 }
 
 function dumpNode(node) {
-    
+    if (!node) {
+        dump("<null>\n");
+        return;
+    }
+    dump(node.text + "\n");
+    dump("right:\n");
+    dumpNode(node.rightPeer);
+    dump("next:\n");
+    dumpNode(node.nextBlock);
+    dump("children:\n");
+    dumpNode(node.firstChild);
+    dump("parent: " + (node.parent ? node.parent.text : "<null>") + "\n");
 }
 
 function dragMouseDown(evt) {
@@ -67,6 +78,8 @@ function dragMouseDown(evt) {
 
                 // HACK
                 if (evt.shiftKey) {
+                    dumpNode(targetElem.blockData);
+                    dump("\n");
                     return;
                 }
                 // HACK
