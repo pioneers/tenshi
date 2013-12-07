87383cb7239fdc2a9eb8d57cd174bc09c074149b
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2013-12-07 11:57:47 -0800

    Created new arbitrary test robot, uprighted this time
    Change-Id: I688f99518f048a876e24656008b1bc9e2f4ffa16

--- angel-player/src/chrome/content/simulator/miscFuncs.js
+++ angel-player/src/chrome/content/simulator/miscFuncs.js
@@ -28,88 +28,88 @@ function loadFrame(simulator)
 function createCar3(simulator)
 {
     var robot = new Robot(simulator, simulator.master);
-    var testShape = new Ammo.btBoxShape(new Ammo.btVector3(1, 1, 5));
+    var testShape = new Ammo.btBoxShape(new Ammo.btVector3(1, 5, 1));
     var cylShape = new Ammo.btCylinderShape(new Ammo.btVector3(6, 1, 6));
     robot.initChassi();
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(5, 10, 1)),
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(5, 1, 10)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
        new Ammo.btVector3(0, 0, 0)),
        100,
-       createBoxMesh(10, 20, 2, 0xaa00aa, simulator.scene));
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(5, 5, 1)),
+       createBoxMesh(10, 2, 20, 0xaa00aa, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 3, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(0, 0, 2)),
-       100,
-       createBoxMesh(10, 10, 2, 0xbb00bb, simulator.scene));
-    robot.addShape(testShape,
-       new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(4, 4, 2 + 5 + 1)),
+       new Ammo.btVector3(0, 2 + 3, 8.5)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 6, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 4, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(4, -4, 2 + 5 + 1)),
+       new Ammo.btVector3(0, 2 + 4, 6)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 8, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 5, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(-4, 4, 2 + 5 + 1)),
+       new Ammo.btVector3(0, 2 + 5, 3)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 10, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 6, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(-4, -4, 2 + 5 + 1)),
+       new Ammo.btVector3(0, 2 + 6, 0)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(5, 6, 1)),
+       createBoxMesh(2, 12, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 5, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(0, 0, 2 + 5 + 5 + 1 + 1)),
+       new Ammo.btVector3(0, 2 + 5, -3)),
        10,
-       createBoxMesh(10, 12, 2, 0x0055555, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 10, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 4, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(3, 3, 2 + 5 + 5 + 1 + 1 + 1 + 5)),
+       new Ammo.btVector3(0, 2 + 4, -6)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 8, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 3, 1)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(3, -3, 2 + 5 + 5 + 1 + 1 + 1 + 5)),
+       new Ammo.btVector3(0, 2 + 3, -8.5)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(2, 6, 2, 0x000044, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(2, 2, 5)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(-3, 3, 2 + 5 + 5 + 1 + 1 + 1 + 5)),
+       new Ammo.btVector3(5, 2 + 2, 0)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(testShape,
+       createBoxMesh(4, 4, 10, 0x222222, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(2, 2, 5)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(-3, -3, 2 + 5 + 5 + 1 + 1 + 1 + 5)),
+       new Ammo.btVector3(-5, 2 + 2, 0)),
        10,
-       createBoxMesh(2, 2, 10, 0xbbbbbb, simulator.scene));
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(4, 4, 1)),
+       createBoxMesh(4, 4, 10, 0x222222, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 1, 3)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(0, 0, 2 + 5 + 5 + 1 + 1 + 1 + 10 + 1)),
+       new Ammo.btVector3(-5, 2 + 2, 5 + 3)),
        10,
-       createBoxMesh(8, 8, 2, 0x664477, simulator.scene));
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(3, 3, 1)),
+       createBoxMesh(2, 2, 6, 0x666666, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(1, 1, 3)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(0, 0, 2 + 5 + 5 + 1 + 1 + 1 + 10 + 2 + 1)),
+       new Ammo.btVector3(5, 2 + 2, 5 + 3)),
        10,
-       createBoxMesh(6, 6, 2, 0x593366, simulator.scene));
-    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(2, 20, 2)),
+       createBoxMesh(2, 2, 6, 0x666666, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(0.5, 0.5, 5)),
        new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
-       new Ammo.btVector3(0, 0, 8)),
+       new Ammo.btVector3(5, 2 + 2, 5 + 6 + 5)),
        10,
-       createBoxMesh(4, 40, 4, 0x110011, simulator.scene));
-    robot.finishChassi(0, 0, 100);
+       createBoxMesh(1, 1, 10, 0x999999, simulator.scene));
+    robot.addShape(new Ammo.btBoxShape(new Ammo.btVector3(0.5, 0.5, 5)),
+       new Ammo.btTransform(new Ammo.btQuaternion(0, 0, 0, 1),
+       new Ammo.btVector3(-5, 2 + 2, 5 + 6 + 5)),
+       10,
+       createBoxMesh(1, 1, 10, 0x999999, simulator.scene));
+    robot.finishChassi(0, 100, 0);
 
     var x = [1, -1, 1, -1];
     var y = [1, 1, -1, -1];
     for(var i = 0; i < 4; i++)
     {
-        temp_motor = new Motor(simulator, 2, 2, 2, 100, 0xff99ff, 3, 2, 100, 0xaabbff, x[i]*5, y[i]*7, 100);
+        temp_motor = new Motor(simulator, 2, 2, 2, 100, 0xff99ff, 3, 2, 100, 0xaabbff, x[i]*5, 50, y[i]*7);
         robot.addMotor(i, temp_motor, temp_motor.motor,
-           new Ammo.btVector3(x[i]*5, y[i]*7, 0),
+           new Ammo.btVector3(x[i]*5, 0, y[i]*7),
            new Ammo.btVector3(0, 0, -1),
            new Ammo.btVector3(x[i]*1, 0, 0),
            new Ammo.btVector3(0, 0, 1));
@@ -118,9 +118,9 @@ function createCar3(simulator)
 
     temp_rf = new Rangefinder(simulator, 1, 1, 1, 1, 0, 100, 0, 150);
     robot.addSensor(0, temp_rf, temp_rf.physicsObject,
-        new Ammo.btVector3(0, 10, 0),
+        new Ammo.btVector3(0, 0, 10),
         new Ammo.btVector3(0, 0, -0.5),
-        new Ammo.btVector3(0, 1, 0),
+        new Ammo.btVector3(0, 0, 1),
         new Ammo.btVector3(0, 0, 1));
     return robot;
 }
