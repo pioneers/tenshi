13d6202117f39e26d8b5c7d43b836480ef717b78
Robert Ou <robertou@pioneers.berkeley.edu>
2014-02-12 22:06:26 -0800

    Initial I2C driver commit
    Change-Id: I1cde029181244babf3eacab7e5c88d50f1b446a4
    Reviewed-on: https://robertou.com/gerrit/232
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

---
+++ controller/inc/driver_glue.h
@@ -0,0 +1,9 @@
+#ifndef INC_DRIVER_GLUE_H_
+#define INC_DRIVER_GLUE_H_
+
+#include "inc/i2c_master.h"
+
+extern i2c_master_module *i2c1_driver;
+extern void i2c1_init(void);
+
+#endif  // INC_DRIVER_GLUE_H_
---
+++ controller/inc/i2c_master.h
@@ -0,0 +1,31 @@
+#ifndef INC_I2C_MASTER_H_
+#define INC_I2C_MASTER_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// TODO(rqou): Define some kind of common sensor interface
+
+typedef struct tag_i2c_master_module {
+  // No stuff here yet
+} i2c_master_module;
+
+// The only possible transactions are send only, receive only, or send then
+// receive.
+#define TRANSACTION_STATUS_QUEUED     1
+#define TRANSACTION_STATUS_SENDING    2
+#define TRANSACTION_STATUS_RECEIVING  3
+#define TRANSACTION_STATUS_DONE       4
+#define TRANSACTION_STATUS_ERROR      5
+
+extern i2c_master_module *i2c_master_init_module(void *periph_base);
+extern void *i2c_issue_transaction(i2c_master_module *module, uint8_t addr,
+  uint8_t *data_out, size_t len_out, uint8_t *data_in, size_t len_in);
+extern int i2c_transaction_status(i2c_master_module *module,
+  void *transaction);
+extern void i2c_handle_interrupt(i2c_master_module *module);
+extern void i2c_handle_interrupt_error(i2c_master_module *module);
+extern int i2c_transaction_finish(i2c_master_module *module,
+  void *transaction);
+
+#endif  // INC_I2C_MASTER_H_
