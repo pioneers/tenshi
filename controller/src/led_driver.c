9706ab20e6097452856ec94607459363c2c8f4f3
Robert Ou <robertou@pioneers.berkeley.edu>
2014-02-15 19:19:19 -0800

    Change over board to Rev. A and add LED driver
    Debug UART support removed because one doesn't exist on the Tenshi board.
    Conditional compilation of it and/or an alternate implementation will be
    added in a later commit.
    
    Change-Id: I35c952ac99c221acaf1a5bd00c1384ec2ecbefd4

---
+++ controller/src/led_driver.c
@@ -0,0 +1,154 @@
+#include "inc/led_driver.h"
+
+#include "inc/FreeRTOS.h"
+#include "inc/pindef.h"
+#include "inc/stm32f4xx.h"
+#include "inc/task.h"
+
+static volatile uint8_t led_driver_current_mode = 0;
+
+#define LED_YELLOW    0b0001
+#define LED_BLUE      0b0010
+#define LED_GREEN     0b0100
+#define LED_RED       0b1000
+
+typedef struct tag_led_driver_pattern_entry {
+  uint8_t led_states;
+  uint8_t num_ticks;
+} led_driver_pattern_entry;
+
+typedef struct tag_led_driver_pattern {
+  uint8_t len;
+  led_driver_pattern_entry *entries;
+} led_driver_pattern;
+
+static led_driver_pattern driver_patterns[] = {
+  {
+    // PATTERN_DEFAULT_CHASER
+    // Each LED on for 1 second
+    .len = 4,
+    .entries =
+    (led_driver_pattern_entry[]) {
+      {
+        .led_states = LED_YELLOW,
+        .num_ticks = 4,
+      }, {
+        .led_states = LED_BLUE,
+        .num_ticks = 4,
+      }, {
+        .led_states = LED_GREEN,
+        .num_ticks = 4,
+      }, {
+        .led_states = LED_RED,
+        .num_ticks = 4,
+      },
+    },
+  },
+};
+
+// Enable GPIO banks and configure I/O mode
+static void led_driver_init_hw(void) {
+  // Enable appropriate GPIO banks
+  RCC->AHB1ENR |= GPIO_BANK_AHB1ENR(PINDEF_RED_LED);
+  RCC->AHB1ENR |= GPIO_BANK_AHB1ENR(PINDEF_GREEN_LED);
+  RCC->AHB1ENR |= GPIO_BANK_AHB1ENR(PINDEF_BLUE_LED);
+  RCC->AHB1ENR |= GPIO_BANK_AHB1ENR(PINDEF_YELLOW_LED);
+
+  // Set on board LED GPIO output
+  CONFIGURE_IO(RED_LED);
+  CONFIGURE_IO(GREEN_LED);
+  CONFIGURE_IO(BLUE_LED);
+  CONFIGURE_IO(YELLOW_LED);
+}
+
+void led_driver_panic(void) {
+  led_driver_init_hw();
+
+  // Turn on all the LEDs
+  GPIO_BANK(PINDEF_RED_LED)->ODR |=
+    (1 << GPIO_PIN(PINDEF_RED_LED));
+  GPIO_BANK(PINDEF_GREEN_LED)->ODR |=
+    (1 << GPIO_PIN(PINDEF_GREEN_LED));
+  GPIO_BANK(PINDEF_BLUE_LED)->ODR |=
+    (1 << GPIO_PIN(PINDEF_BLUE_LED));
+  GPIO_BANK(PINDEF_YELLOW_LED)->ODR |=
+    (1 << GPIO_PIN(PINDEF_YELLOW_LED));
+}
+
+static portTASK_FUNCTION_PROTO(led_driver_task, pvParameters) {
+  uint8_t current_led_pattern = 0;
+  uint8_t prev_led_pattern = 0;
+  uint8_t pattern_step_index = 0;
+  uint8_t current_step_ticks = 0;
+
+  while (1) {
+    current_led_pattern = led_driver_current_mode;
+
+    // New pattern --> reinit
+    if (current_led_pattern != prev_led_pattern) {
+      prev_led_pattern = current_led_pattern;
+      pattern_step_index = 0;
+      current_step_ticks = 0;
+    }
+
+    // Set/clear the appropriate LEDs
+    uint8_t led_states = driver_patterns[current_led_pattern]
+      .entries[pattern_step_index].led_states;
+    if (led_states & LED_YELLOW) {
+      GPIO_BANK(PINDEF_YELLOW_LED)->ODR |=
+        (1 << GPIO_PIN(PINDEF_YELLOW_LED));
+    } else {
+      GPIO_BANK(PINDEF_YELLOW_LED)->ODR &=
+        ~(1 << GPIO_PIN(PINDEF_YELLOW_LED));
+    }
+    if (led_states & LED_BLUE) {
+      GPIO_BANK(PINDEF_BLUE_LED)->ODR |=
+        (1 << GPIO_PIN(PINDEF_BLUE_LED));
+    } else {
+      GPIO_BANK(PINDEF_BLUE_LED)->ODR &=
+        ~(1 << GPIO_PIN(PINDEF_BLUE_LED));
+    }
+    if (led_states & LED_GREEN) {
+      GPIO_BANK(PINDEF_GREEN_LED)->ODR |=
+        (1 << GPIO_PIN(PINDEF_GREEN_LED));
+    } else {
+      GPIO_BANK(PINDEF_GREEN_LED)->ODR &=
+        ~(1 << GPIO_PIN(PINDEF_GREEN_LED));
+    }
+    if (led_states & LED_RED) {
+      GPIO_BANK(PINDEF_RED_LED)->ODR |=
+        (1 << GPIO_PIN(PINDEF_RED_LED));
+    } else {
+      GPIO_BANK(PINDEF_RED_LED)->ODR &=
+        ~(1 << GPIO_PIN(PINDEF_RED_LED));
+    }
+
+    // Increment tick/advance step
+    if (++current_step_ticks ==
+      driver_patterns[current_led_pattern]
+      .entries[pattern_step_index].num_ticks) {
+      current_step_ticks = 0;
+      // Increment/reset step
+      if (++pattern_step_index == driver_patterns[current_led_pattern].len) {
+        pattern_step_index = 0;
+      }
+    }
+
+    // Sleep for 1/4 second (tick time for LED patterns)
+    vTaskDelay(250 / portTICK_RATE_MS);
+  }
+}
+
+void led_driver_init(void) {
+  led_driver_init_hw();
+
+  xTaskCreate(led_driver_task, "LEDs", 256, NULL, tskIDLE_PRIORITY, NULL);
+}
+
+void led_driver_set_mode(uint8_t mode) {
+  led_driver_current_mode = mode;
+}
+
+uint8_t led_driver_get_mode(void) {
+  return led_driver_current_mode;
+}
