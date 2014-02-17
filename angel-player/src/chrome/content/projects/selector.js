e1ee554c12531228d84108f9481b2294f5885d0b
Nikita Kitaev <nikitakitaev@pioneers.berkeley.edu>
2014-02-17 10:29:43 -0800

    Naive implementation of save/load functionality
    Change-Id: If54483095e26a74539b2203e6b38316c7a675b2b

---
+++ angel-player/src/chrome/content/projects/selector.js
@@ -0,0 +1,63 @@
+var storage = window.localStorage;
+
+var serializers = {},
+    deserializers = {};
+
+function connect_storage(key, serialize, deserialize) {
+  serializers[key] = serialize;
+  deserializers[key] = deserialize;
+  deserialize(window.localStorage[key]);
+}
+
+function deserialize_all() {
+  for (var key in deserializers) {
+    if (deserializers.hasOwnProperty(key)) {
+      deserializers[key](window.localStorage[key]);
+    }
+  }
+  update_screen();
+}
+
+function serialize_all() {
+  for (var key in serializers) {
+    if (serializers.hasOwnProperty(key)) {
+      window.localStorage[key] = serializers[key]();
+    }
+  }
+  window.localStorage.date = Date();
+  update_screen();
+}
+
+function reset_all() {
+  for (var key in deserializers) {
+    if (deserializers.hasOwnProperty(key)) {
+      deserializers[key](undefined);
+    }
+  }
+  update_screen("Reset to defaults. ");
+}
+
+function clear_save() {
+  window.localStorage.clear();
+  update_screen();
+}
+
+function update_screen() {
+  var prefix = "";
+  if (arguments.length > 0) {
+    prefix = arguments[0];
+  }
+  var date = window.localStorage.date;
+  if (date === undefined) {
+    $("#text").text(prefix + "No saves available");
+  } else {
+    $("#text").text(prefix + "Last saved: " + date);
+  }
+}
+
+var exports = {};
+exports.connect_storage = connect_storage;
+exports.deserialize_all = deserialize_all;
+exports.serialize_all = serialize_all;
+exports.reset_all = reset_all;
+exports.clear_save = clear_save;
