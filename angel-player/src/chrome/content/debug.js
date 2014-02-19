79c6c65358862f404327071d4958b3854b164521
Robert Ou <robertou@pioneers.berkeley.edu>
2014-02-19 11:53:30 -0800

    Enable XULRunner remote debugger
    It is now possible to connect to angel-player with the remote debug tool
    in Firefox. Note that it is NOT enabled in the release builds because
    the devtools.debugger.remote-enabled is not set to true in the release
    builds.
    
    Change-Id: I29a94e22e856f173678c4092d004799aee12bb34

--- angel-player/src/chrome/content/debug.js
+++ angel-player/src/chrome/content/debug.js
@@ -1,3 +1,4 @@
+Components.utils.import('resource://gre/modules/devtools/dbg-server.jsm');
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 var EXPORTED_SYMBOLS = ["debugModule"];
@@ -5,10 +6,18 @@ var EXPORTED_SYMBOLS = ["debugModule"];
 var debugModule = {};
 
 var DEBUG_MODE_PREF = "tenshi.enableDebug";
+var MAIN_WINDOW_TYPE = "angel-player:main"
 var debugMode = false;
 
 debugModule.init = function() {
     debugMode = Services.prefs.getBoolPref(DEBUG_MODE_PREF);
+
+    // Start remote debugger
+    if (!DebuggerServer.initialized) {
+      DebuggerServer.init();
+      DebuggerServer.addBrowserActors(MAIN_WINDOW_TYPE);
+    }
+    DebuggerServer.openListener(6000);
 };
 
 debugModule.isDebugEnabled = function() {
