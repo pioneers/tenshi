2d5bb17f482264c406a6c912e0db1d2440eb4e24
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-02-22 21:51:18 -0800

    Updated compiler to use opcodes from spec.
    Change-Id: Id69adbb76143ce0d17f50fe9f5904fe96a61d515

---
+++ angel-player/src/chrome/content/angelic/doc/instruction_set.md
@@ -0,0 +1,314 @@
+Angelic VM Spec
+===============
+# Opcodes
+Opcodes are 7 bit numbers mapping to particular functions in the Angelic VM.
+The top (8th) bit is used to control value inspection in the C implementation.
+
+## Opcode Loading and Evalutation
+Opcodes are loaded in groups of 4 (32 bits) called "bunches." As opcodes are evaluated, the other opcodes are removed from the bunch, and 0s are added at the end. Opcodes may also consume some fixed number of bytes from the bunch, indicated in the rest of this documentation as `[opcode_name arg1 ... argn]`. Instructions should not rely on bytes of 0 being fed into the bunch as opcodes are evaluated (the compiler should not, for example, emit a `li1` at the end of a bunch to load a 0). This implies that opcodes should not receive more than 3 bytes of arguments in this manner.
+Opcodes may also remove whole bunches (multiples of 32 bits) from the opcode stream. This is indicated in the rest of this document as `[opcode_name] [arg]`. The number of bunches consumed in this way need not be bounded or fixed at a particular value per opcode.
+
+If the top bit of an opcode is set, the evaluation of that opcode is logged in some VM specific way.
+## Angelic VM Instruction Set
+This is the number to opcode mapping.
+
+<code>
+
+  * 0x00 : next
+  * 0x01 : dup1
+  * 0x02 : liw
+  * 0x03 : set1
+  * 0x04 : pop
+  * 0x05 : call1
+  * 0x06 : ret
+  * 0x07 : eq
+  * 0x08 : j1
+  * 0x09 : j2
+  * 0x0a : j3
+  * 0x0b : jw
+  * 0x0c : bz1
+  * 0x0d : bz2
+  * 0x0e : bz3
+  * 0x0f : bzw
+  * 0x10 : not
+  * 0x11 : fadd
+  * 0x12 : fsub
+  * 0x13 : fmul
+  * 0x14 : fdiv
+  * 0x15 : fmod
+  * 0x16 : iadd
+  * 0x17 : isub
+  * 0x18 : imul
+  * 0x19 : idiv
+  * 0x1a : imod
+  * 0x1b : uadd
+  * 0x1c : usub
+  * 0x1d : umul
+  * 0x1e : udiv
+  * 0x1f : umod
+  * 0x20 : refi
+  * 0x21 : refd
+  * 0x22 : make1
+  * 0x23 : pushfree1
+  * 0x24 : popfree1
+  * 0x25 : clone
+  * 0x26 : safe
+  * 0x27 : read1
+  * 0x28 : write1
+  * 0x29 : stack1
+  * 0x2a : noop
+  * 0x2b : end
+  * 0x2c : debug
+  * 0x2d : f2i
+  * 0x2e : i2f
+  * 0x2f : band
+  * 0x30 : bor
+  * 0x31 : bxor
+  * 0x32 : bnot
+  * 0x33 : bsl
+  * 0x34 : bsrl
+  * 0x35 : bsra
+  * 0x36 : reserved
+  * 0x37 : reserved
+  * 0x38 : reserved
+  * 0x39 : reserved
+  * 0x3a : reserved
+  * 0x3b : reserved
+  * 0x3c : reserved
+  * 0x3d : reserved
+  * 0x3e : reserved
+  * 0x3f : reserved
+  * 0x40 : reserved
+  * 0x41 : reserved
+  * 0x42 : reserved
+  * 0x43 : reserved
+  * 0x44 : reserved
+  * 0x45 : reserved
+  * 0x46 : reserved
+  * 0x47 : reserved
+  * 0x48 : reserved
+  * 0x49 : reserved
+  * 0x4a : reserved
+  * 0x4b : reserved
+  * 0x4c : reserved
+  * 0x4d : reserved
+  * 0x4e : reserved
+  * 0x4f : reserved
+  * 0x50 : reserved
+  * 0x51 : reserved
+  * 0x52 : reserved
+  * 0x53 : reserved
+  * 0x54 : reserved
+  * 0x55 : reserved
+  * 0x56 : reserved
+  * 0x57 : reserved
+  * 0x58 : reserved
+  * 0x59 : reserved
+  * 0x5a : reserved
+  * 0x5b : reserved
+  * 0x5c : reserved
+  * 0x5d : reserved
+  * 0x5e : reserved
+  * 0x5f : reserved
+  * 0x60 : reserved
+  * 0x61 : reserved
+  * 0x62 : reserved
+  * 0x63 : reserved
+  * 0x64 : reserved
+  * 0x65 : reserved
+  * 0x66 : reserved
+  * 0x67 : reserved
+  * 0x68 : reserved
+  * 0x69 : reserved
+  * 0x6a : reserved
+  * 0x6b : reserved
+  * 0x6c : reserved
+  * 0x6d : reserved
+  * 0x6e : reserved
+  * 0x6f : reserved
+  * 0x70 : reserved
+  * 0x71 : reserved
+  * 0x72 : reserved
+  * 0x73 : reserved
+  * 0x74 : reserved
+  * 0x75 : reserved
+  * 0x76 : reserved
+  * 0x77 : reserved
+  * 0x78 : reserved
+  * 0x79 : reserved
+  * 0x7a : reserved
+  * 0x7b : reserved
+  * 0x7c : reserved
+  * 0x7d : reserved
+  * 0x7e : reserved
+  * 0x7f : reserved
+</code>
+
+### [next]
+Causes the next bunch of opcodes to be loaded. Any further opcodes in this bunch will be skipped.
+
+### [dup1 *idx*]
+Duplicates the value *idx* places from the top of stack.
+
+### [liw] [*val*]
+Causes *val* to be pushed to the top of the stack.
+
+### [set1 *idx*]
+Causes the top value of the stack to be written into *idx* from the top of stack. Pop's the top value from the stack.
+
+### [pop]
+Removes the top value from the stack.
+
+### [call1 *argc*]
+Performs a function call, where the top *argc* arguments on the stack are arguments, and *argc* + 1 is the location of the function to call.
+
+### [ret]
+Return from a function call. Saves and restores the top value on the stack.
+If there are no further functions to return to, stops the vm.
+
+### [eq]
+Pops the top two values on the stack, and pushes whether they are equal.
+
+### [j1 *relative_dest*]
+Causes PC to be moved by *relative_dest*, a signed 8 bit integer.
+
+### [j2 *relative_dest0* *relative_dest1*]
+Causes PC to be moved by *relative_dest0*:*relative_dest1*, a signed 16 bit integer.
+
+### [j3 *relative_dest0* *relative_dest1* *relative_dest2*]
+Causes PC to be moved by *relative_dest0*:*relative_dest1*:*relative_dest2*, a signed 24 bit integer.
+
+### [jw] [*relative_dest*]
+Causes PC to be moved by *relative_dest*, a signed 32 bit integer.
+
+### [bz1 *relative_dest*]
+Pops the top value on the stack. If that value is zero, causes PC to be moved by *relative_dest*, a signed 8 bit integer.
+
+### [bz2 *relative_dest0* *relative_dest1*]
+Pops the top value on the stack. If that value is zero, causes PC to be moved by *relative_dest0*:*relative_dest1*, a signed 16 bit integer.
+
+### [bz3 *relative_dest0* *relative_dest1* *relative_dest2*]
+Pops the top value on the stack. If that value is zero, causes PC to be moved by *relative_dest0*:*relative_dest1*:*relative_dest2*, a signed 24 bit integer.
+
+### [bzw] [*relative_dest*]
+Pops the top value on the stack. If that value is zero, causes PC to be moved by *relative_dest*, a signed 32 bit integer.
+
+### [not]
+Pops the top value on the stack. If it is 0, pushes 1. Otherwise pushes 0.
+
+### [fadd]
+Pops the top two values on the stack. Pushes the sum of these values.
+
+### [fsub]
+Pops the top two values on the stack. Pushes the difference of these values.
+
+### [fmul]
+Pops the top two values on the stack. Pushes the product of these values.
+
+### [fdiv]
+Pops the top two values on the stack. Pushes the quotient of these values.
+
+### [fmod]
+Pops the top two values on the stack. Pushes the first number reduced modulo the second.
+
+### [iadd]
+Pops the top two values on the stack. Pushes the sum of these values.
+
+### [isub]
+Pops the top two values on the stack. Pushes the difference of these values.
+
+### [imul]
+Pops the top two values on the stack. Pushes the product of these values.
+
+### [idiv]
+Pops the top two values on the stack. Pushes the quotient of these values.
+
+### [imod]
+Pops the top two values on the stack. Pushes the first number reduced modulo the second.
+
+### [uadd]
+Pops the top two values on the stack. Pushes the sum of these values.
+
+### [usub]
+Pops the top two values on the stack. Pushes the difference of these values.
+
+### [umul]
+Pops the top two values on the stack. Pushes the product of these values.
+
+### [udiv]
+Pops the top two values on the stack. Pushes the quotient of these values.
+
+### [umod]
+Pops the top two values on the stack. Pushes the first number reduced modulo the second.
+
+### [refi]
+Increments the reference count of the val on top of stack.
+
+### [refd]
+Decrements the reference count of the val on top of stack. Frees if the reference count became 0. Pop's the value off the stack.
+
+### [make1 *argc*]
+Constructs an object, where the top *argc* arguments on the stack are fields, and *argc* + 1 is the location of the type to create.
+
+### [pushfree1 *count*]
+Push the offset of the top *count* frame location from the top of stack onto the freestack. Intended to be used only for local variables and the destructuring objects.
+
+### [popfree1 *count*]
+Pop the top *count* offsets from the freestack, decrementing the references at those locations. Intended to be used only for local variables and destructuring objects.
+
+### [clone]
+Pop the top object off the stack. Duplicate it and push the duplicate.
+
+### [safe]
+Indicates a safe point. The vm may suspend here, update external data sctructure, etc.
+
+### [read1 *idx*]
+Read the field of the object on the top of the stack at index *idx*. *idx* == 1 is the index of the type tag in most objects, and the actual fields start at *idx* == 2.
+Pops the object off the stack.
+
+### [write1 *idx*]
+Write the second value on the stack to the object on the top of the stack at index *idx*. *idx* == 1 is the index of the type tag in most objects, and the actual fields start at *idx* == 2.
+Pops both the object and value off the stack.
+
+### [stack1 *diff*]
+Move the stack up or down by *diff*. Note that the pop opcode is a special case, where *diff* = -1.
+
+### [noop]
+Does nothing.
+
+### [end]
+Stops the VM immediately.
+
+### [debug]
+Unspecified debugging related behavior.
+
+### [f2i]
+Pop's the top value from the stack (a float) and converts it to an integer.
+
+### [i2f]
+Pop's the top value from the stack (an integer) and converts it to an float.
+
+### [band]
+Pop's the top two values from the stack (both integers) and pushed the binary and of the two.
+
+### [bor]
+Pop's the top two values from the stack (both integers) and pushed the binary or of the two.
+
+### [bxor]
+Pop's the top two values from the stack (both integers) and pushed the binary xor of the two.
+
+### [bnot]
+Pop's the top value from the stack (an integer) and pushed the binary inverse of it.
+
+### [bsl]
+Pop's the top two values from the stack (both integers) and pushed the the second value bit shifted to the left by the first value.
+
+### [bsrl]
+Pop's the top two values from the stack (both integers) and pushed the the second value bit shifted to the right by the first value, without doing sign extension.
+
+### [bsra]
+Pop's the top two values from the stack (both integers) and pushed the the second value bit shifted to the right by the first value, doing sign extension.
+
+### [reserved]
+Behavior may be defined at a later date.
+
--- angel-player/src/chrome/content/angelic/opcodes.js
+++ angel-player/src/chrome/content/angelic/opcodes.js
@@ -11,7 +11,7 @@ function make_opcode ( code, name, argc, stack, func ) {
   }
 
 var opcodes = [
-  make_opcode ( 0, 'noop', 0, 0,
+  make_opcode ( 0x00, 'next', 0, 0,
   function noop ( state ) {
     // Since noop's are shifted into the bunch, or used to pad bunches, as soon
     // as a noop is encountered, the next bunch needs to be loaded.
@@ -25,78 +25,28 @@ var opcodes = [
       state.bunch = state.func[state.get_pc ( ) ].slice ( 0 );
       state.move_pc ( 1 );
       }
-    }),
-  make_opcode ( 1, 'add', 1, -1,
-  function add ( state ) {
-    state.push ( state.pop ( ) + state.pop ( ) );
-    }),
-  make_opcode ( 2, 'li', 1, 1,
-  function li ( state ) {
+    } ),
+  make_opcode ( 0x01, 'dup1', 2, 1,
+    function dup1 ( state ) {
+    state.push ( state.get_stack ( state.get_arg ( 1 ) ) );
+    } ),
+  make_opcode ( 0x02, 'liw', 1, 1,
+  function liw ( state ) {
     state.push ( state.func[ state.get_pc ( ) ] );
     state.move_pc ( 1 );
-    }),
-  make_opcode ( 3, 'print', 2, 0,
-  function print ( state ) {
-    // This exists because we have not implemented external function calls yet.
-    var arg_count = state.get_arg ( 1 );
-    var i;
-    var stack_length = state.stack.length;
-
-    for ( i = 0; i < arg_count; i++ ) {
-      misc.print ( 'VM:' + state.stack[stack_length - i - 1] );
-      }
-    }),
-  make_opcode ( 4, 'end', 1, 0,
-  function end ( state ) {
-    state.run = false;
-    }),
-  make_opcode ( 5, 'mult', 1, -1,
-  function add ( state ) {
-    state.push ( state.pop ( ) * state.pop ( ) );
-    }),
-  make_opcode ( 6, 'div', 1, -1,
-  function add ( state ) {
-    state.push ( state.pop ( ) / state.pop ( ) );
-    }),
-  make_opcode ( 7, 'bn1', 2, -1,
-  function bn1 ( state ) {
-    if ( ! state.pop ( ) ) {
-      state.move_pc ( state.get_arg ( 1 ) );
-      }
-    }),
-  make_opcode ( 8, 'eq', 1, -1,
-  function eq ( state ) {
-    state.push ( state.pop ( ) === state.pop ( ) );
-    }),
-  make_opcode ( 9, 'dup', 2, 1,
-    function dup ( state ) {
-    state.push ( state.get_stack ( state.get_arg ( 1 ) ) );
-    }),
-  make_opcode ( 10, 'set', 2, -1,
+    } ),
+  make_opcode ( 0x03, 'set1', 2, -1,
     function set ( state ) {
     state.set_stack ( state.get_arg ( 1 ), state.pop ( ) );
-    }),
-  make_opcode ( 11, 'not', 1, 0,
-  function not ( state ) {
-    state.push ( ! state.pop ( ) );
-    }),
-  make_opcode ( 12, 'j1', 2, 0,
-  function j1 ( state ) {
-    state.move_pc ( state.get_arg ( 1 ) );
-    }),
-  make_opcode ( 13, 'pop', 1, -1,
+    } ),
+  make_opcode ( 0x04, 'pop', 1, -1,
   function pop ( state ) {
     state.pop ( );
-    }),
-  make_opcode ( 14, 'sub', 1, -1,
-  function sub ( state ) {
-    state.push ( -state.pop ( ) + state.pop ( ) );
-    }),
-  make_opcode ( 15, 'call', 2, 0,
+    } ),
+  make_opcode ( 0x05, 'call1', 2, 0,
   function call ( state ) {
     var arg_count = state.get_arg ( 1 );
     var func = state.get_stack ( arg_count );
-    //misc.print ( 'func', func );
     if ( func.type === 'external' ) {
       var size = state.stack.length;
       var args = state.stack.splice ( size - arg_count, size );
@@ -118,8 +68,8 @@ var opcodes = [
       state.func = func.code;
       state._pc = 0;
       }
-    }),
-  make_opcode ( 16, 'ret', 1, 0,
+    } ),
+  make_opcode ( 0x06, 'ret', 1, 0,
   function ret ( state ) {
     if ( state.call_stack_top === 0 ) {
       state.run = false;
@@ -135,7 +85,299 @@ var opcodes = [
       state.push ( out );
       state.clear_bunch ( );
       }
-    }),
+    } ),
+  make_opcode ( 0x07, 'eq', 1, -1,
+  function eq ( state ) {
+    state.push ( state.pop ( ) === state.pop ( ) );
+    } ),
+  make_opcode ( 0x08, 'j1', 2, 0,
+  function j1 ( state ) {
+    state.move_pc ( state.get_arg ( 1 ) );
+    } ),
+  make_opcode ( 0x09, 'j2', 3, 0,
+  function j2 ( state ) {
+    state.move_pc ( state.get_arg ( 1 ) << 8 + state.get_arg ( 2 ) );
+    } ),
+  make_opcode ( 0x0a, 'j3', 4, 0,
+  function j3 ( state ) {
+    state.move_pc ( ( state.get_arg ( 1 ) << 8 +
+                      state.get_arg ( 2 ) << 8 ) +
+                      state.get_arg ( 3 ) );
+    } ),
+  make_opcode ( 0x0b, 'jw', 1, 0,
+  function jw ( state ) {
+    state.move_pc ( state.func[ state.get_pc ( ) ] );
+    } ),
+  make_opcode ( 0x0c, 'bz1', 2, -1,
+  function bz1 ( state ) {
+    if ( ! state.pop ( ) ) {
+      state.move_pc ( state.get_arg ( 1 ) );
+      }
+    } ),
+  make_opcode ( 0x0d, 'bz2', 3, -1,
+  function bz2 ( state ) {
+    if ( ! state.pop ( ) ) {
+      state.move_pc ( state.get_arg ( 1 ) << 8 + state.get_arg ( 2 ) );
+      }
+    } ),
+  make_opcode ( 0x0e, 'bz3', 4, -1,
+  function bz3 ( state ) {
+    if ( ! state.pop ( ) ) {
+    state.move_pc ( ( state.get_arg ( 1 ) << 8 +
+                      state.get_arg ( 2 ) << 8 ) +
+                      state.get_arg ( 3 ) );
+      }
+    } ),
+  make_opcode ( 0x0f, 'bzw', 1, -1,
+  function bzw ( state ) {
+    if ( ! state.pop ( ) ) {
+      state.move_pc ( state.func[ state.get_pc ( ) ] );
+      }
+    } ),
+  make_opcode ( 0x10, 'not', 1, 0,
+  function not ( state ) {
+    state.push ( ! state.pop ( ) );
+    } ),
+
+  make_opcode ( 0x11, 'fadd', 1, -1,
+  function fadd ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left + right );
+    } ),
+  make_opcode ( 0x12, 'fsub', 1, -1,
+  function fsub ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left - right );
+    } ),
+  make_opcode ( 0x13, 'fmul', 1, -1,
+  function fmul ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left * right );
+    } ),
+  make_opcode ( 0x14, 'fdiv', 1, -1,
+  function fdiv ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left / right );
+    } ),
+  make_opcode ( 0x15, 'fmod', 1, -1,
+  function fmod ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left % right );
+    } ),
+
+  make_opcode ( 0x16, 'iadd', 1, -1,
+  function iadd ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left + right );
+    } ),
+  make_opcode ( 0x17, 'isub', 1, -1,
+  function isub ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left - right );
+    } ),
+  make_opcode ( 0x18, 'imul', 1, -1,
+  function imul ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left * right );
+    } ),
+  make_opcode ( 0x19, 'idiv', 1, -1,
+  function idiv ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left / right );
+    } ),
+  make_opcode ( 0x1a, 'imod', 1, -1,
+  function imod ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left % right );
+    } ),
+
+  make_opcode ( 0x1b, 'uadd', 1, -1,
+  function uadd ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left + right );
+    } ),
+  make_opcode ( 0x1c, 'usub', 1, -1,
+  function usub ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left - right );
+    } ),
+  make_opcode ( 0x1d, 'umul', 1, -1,
+  function umul ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left * right );
+    } ),
+  make_opcode ( 0x1e, 'udiv', 1, -1,
+  function udiv ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left / right );
+    } ),
+  make_opcode ( 0x1f, 'umod', 1, -1,
+  function umod ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left % right );
+    } ),
+
+  make_opcode ( 0x20, 'refi', 1, 0,
+  function refi ( state ) {
+    // TODO(kzentner): Do we want to emulate reference counting in javascript.
+    } ),
+  make_opcode ( 0x21, 'refd', 1, 0,
+  function refd ( state ) {
+    // TODO(kzentner): Do we want to emulate reference counting in javascript.
+    state.pop ( );
+    } ),
+
+  make_opcode ( 0x22, 'make1', 2, 0,
+  function make1 ( state ) {
+    var field_count = state.get_arg ( 1 );
+    var size = state.stack.length;
+    var obj = state.stack.splice ( size - field_count - 1, size );
+
+    // Pop the fields.
+    for ( var i = 0; i < field_count; i++ ) {
+      state.pop ( );
+      }
+
+    // Pop the type tag.
+    state.pop ( );
+
+    state.push ( obj );
+    } ),
+
+  make_opcode ( 0x23, 'pushfree1', 1, 0,
+  function pushfree1 ( state ) {
+    // TODO(kzentner): Do we want to emulate reference counting in javascript?
+    } ),
+
+  make_opcode ( 0x24, 'popfree1', 1, 0,
+  function popfree1 ( state ) {
+    // TODO(kzentner): Do we want to emulate reference counting in javascript?
+    } ),
+
+  make_opcode ( 0x25, 'clone', 1, 0,
+  function pushfree1 ( state ) {
+    function copy ( obj ) {
+      if ( typeof ( obj ) !== 'object' ) {
+        return obj;
+        }
+      out = [];
+      obj.forEach ( function ( val ) {
+        // TODO(kzentner): Only recurse on mutable objects.
+        out.push ( copy ( val ) );
+        } );
+      return out;
+      }
+    state.push ( copy ( state.pop ( ) ) );
+    } ),
+
+  make_opcode ( 0x26, 'safe', 1, 0,
+  function safe ( state ) {
+    // TODO(kzentner): Implement safe point instrumentation.
+    } ),
+
+  make_opcode ( 0x27, 'read1', 2, 0,
+  function read1 ( state ) {
+    state.push ( state.pop ( )[ state.get_arg ( 1 ) - 1 ] );
+    } ),
+
+  make_opcode ( 0x28, 'write1', 2, 0,
+  function write1 ( state ) {
+    var val = state.pop ( );
+    var obj = state.pop ( );
+    obj[ state.get_arg ( 1 ) - 1 ] = val;
+    } ),
+
+  make_opcode ( 0x29, 'stack1', 2, 0,
+  function stack1 ( state ) {
+    state.stack_top += state.get_arg ( 1 );
+    } ),
+
+  make_opcode ( 0x2a, 'noop', 1, 0,
+  function noop ( state ) {
+    } ),
+
+  make_opcode ( 0x2b, 'end', 1, 0,
+  function end ( state ) {
+    state.run = false;
+    } ),
+
+  make_opcode ( 0x2c, 'debug', 1, 0,
+  function debug ( state ) {
+    state.debug = ! state.debug;
+    } ),
+
+  make_opcode ( 0x2d, 'f2i', 1, 0,
+  function f2i ( state ) {
+    // TODO(kzentner): Implement integer arithmetic in the vm.
+    } ),
+
+  make_opcode ( 0x2e, 'i2f', 1, 0,
+  function i2f ( state ) {
+    // TODO(kzentner): Implement integer arithmetic in the vm.
+    } ),
+
+  make_opcode ( 0x2f, 'band', 1, 0,
+  function band ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left & right );
+    } ),
+
+  make_opcode ( 0x30, 'bor', 1, 0,
+  function band ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left | right );
+    } ),
+
+  make_opcode ( 0x31, 'bxor', 1, 0,
+  function bxor ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left ^ right );
+    } ),
+
+  make_opcode ( 0x32, 'bnot', 1, 0,
+  function bnot ( state ) {
+    state.push ( ~state.pop ( ) );
+    } ),
+
+  make_opcode ( 0x33, 'bsl', 1, 0,
+  function bsl ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left << right );
+    } ),
+
+  make_opcode ( 0x34, 'bsrl', 1, 0,
+  function bsra ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left >>> right );
+    } ),
+
+  make_opcode ( 0x35, 'bsra', 1, 0,
+  function bsrl ( state ) {
+    var right = state.pop ( );
+    var left = state.pop ( );
+    state.push ( left >> right );
+    } ),
+
   ];
 
 function make_optables ( ops ) {
