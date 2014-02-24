c882fb900269771a2959f52c1801034e1f2b923e
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-02-24 15:53:01 -0800

    Add tests for binary.js
    Change-Id: I10d29e24a62892ea276b9bdf4606bd14df633b90

---
+++ angel-player/src/chrome/content/angelic/tests/binary/correct_contents.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import subprocess
+import sys
+import os
+
+node = subprocess.Popen(['node',
+                         os.path.join(sys.argv[2],
+                                      'correct_contents',
+                                      'correct_contents.js'),
+                         sys.argv[1]])
+
+node.communicate()
+
+with open('correct_contents.bin', 'br') as f:
+    buf = f.read()
+    assert len(buf) == 1024
+    for index, byte in enumerate(buf):
+        assert index & 0xff == byte
+
+sys.exit(node.returncode)
