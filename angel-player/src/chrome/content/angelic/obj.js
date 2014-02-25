5aade85e682e9df350848ed05db5994171c6583f
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-02-25 09:30:27 -0800

    Added assembler.js for new output pass.
    Change-Id: I11c482b27d55416dc381d4bddfe37caea4345400

--- angel-player/src/chrome/content/angelic/obj.js
+++ angel-player/src/chrome/content/angelic/obj.js
@@ -1,11 +1,17 @@
+var string_map = require ( './string_map.js' );
+
 var make = function make ( name ) {
   if ( name === undefined ) {
     name = '#obj';
     }
   return {
-    name: name,
-    data: [],
-    relocations: [],
+    name : name,
+    data : [],
+    relocations : [],
+    id : 0,
+    last_label_id : 0,
+    last_stack_id : 0,
+    labels : string_map.make ( ),
     };
   };
 
