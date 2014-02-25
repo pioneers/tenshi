91db17734195f2024fafffd20f528cab89aec9fd
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-02-24 16:08:04 -0800

    Moved angelic integration test to a test file.
    Change-Id: Ifc493e1617210b53abbbf8568dc0904c60e15131
    Reviewed-on: https://robertou.com/gerrit/270
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/angelic/robot.js
+++ angel-player/src/chrome/content/angelic/robot.js
@@ -3,7 +3,6 @@ var angelic = require ( './main.js' );
 
 //
 // This is the main Angelic module
-// Currently, it contains some test code.
 //
 
 function compile_and_run ( text, robot ) {
@@ -25,45 +24,6 @@ function compile_and_run ( text, robot ) {
   vm.set_evaluation_rate ( 100, 0.01 );
   return vm;
   }
-
-var to_parse = '' +
-'fib = fn (n):\n' +
-'    n = n - 1\n' +
-'    a = 0\n' +
-'    b = 1\n' +
-'    while n != 0:\n' +
-'        temp = a + b\n' +
-'        a = b\n' +
-'        b = temp\n' +
-'        n = n - 1\n' +
-'    return b\n' +
-//'recfib = fn (n):\n' +
-//'    if n == 0:\n' +
-//'        return 0\n' +
-//'    if n == 1:\n' +
-//'        return 1\n' +
-//'    else:\n' +
-//'        return recfib (n - 1) + recfib (n - 2)\n' +
-'main = fn:\n' +
-//'     while 1 != 0:\n' +
-//'         print(1337)\n' +
-'    x = 50\n' +
-'    set_motor (0, 100)\n' +
-'    set_motor (1, 100)\n' +
-'    print (x)\n' +
-'    fib50 = fib (x)\n' +
-//'    fib10 = recfib (10)\n' +
-'    print (fib50)\n' +
-'    g = get_sensor(0)\n' +
-'    print (g)\n' +
-'    print (x)\n' +
-//'    print (fib10)\n' +
-//'    set_motor (0, 20)\n' +
-//'    set_motor (1, 20)\n' +
-'';
-
-var robot = { motors: {}, sensors: { 0: 100 }, version: 0 };
-compile_and_run ( to_parse, robot );
 
 exports.compile_and_run = compile_and_run;
 exports.make = angelic.make;
