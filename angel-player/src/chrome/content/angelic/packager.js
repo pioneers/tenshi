76631002607ec8600dd1f0c94b2cd7a49da8cddb
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-03-04 23:14:05 -0800

    Added branch and jump expansion pass.
    Change-Id: I5b08f1912d9f5239782893d7558816c70d081aff
    Reviewed-on: https://robertou.com/gerrit/291
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/angelic/packager.js
+++ angel-player/src/chrome/content/angelic/packager.js
@@ -1,5 +1,6 @@
 var buffer = require ( 'buffer' );
 var misc = require ( './misc.js' );
+var opcodes = require ( './opcodes.js' );
 
 function make_row ( type, val ) {
   return { type : type, val : val };
@@ -12,6 +13,109 @@ function expand_li ( rows ) {
     if ( row.type === 'li' ) {
       out.push ( make_row ( 'cmd', [ 'li_w', [ ] ] ) );
       out.push ( make_row ( 'literal', row.val ) );
+      }
+    else {
+      out.push ( row );
+      }
+    }
+  return out;
+  }
+
+function max_size_row ( row ) {
+  if ( row.type === 'cmd' ) {
+    return 1;
+    }
+  else if ( row.type === 'literal' ) {
+    return 2;
+    }
+  else if ( row.type === 'bz' ) {
+    return 2;
+    }
+  else if ( row.type === 'j' ) {
+    return 2;
+    }
+  else {
+    return 0;
+    }
+  }
+
+function max_distance ( rows, start, label ) {
+  var dist = 0;
+  var i;
+  var row;
+
+  for ( i = start; i < rows.length; i++ ) {
+    row = rows[i];
+    if ( row.type === 'label' && row.val === label ) {
+      return dist;
+      }
+    dist += max_size_row ( row );
+    }
+
+  // Didn't find it going forwards, search backwards.
+  dist = 0;
+  for ( i = start; i >= 0; i-- ) {
+    row = rows[i];
+    if ( row.type === 'label' && row.val === label ) {
+      return dist;
+      }
+    dist += max_size_row ( row );
+    }
+
+  throw 'Could not find label ' + label;
+  }
+
+function expand_label_1 ( label ) {
+  return [ label + '[0]' ];
+  }
+
+function expand_label_2 ( label ) {
+  return [ label + '[0]', label + '[1]' ];
+  }
+
+function expand_label_3 ( label ) {
+  return [ label + '[0]', label + '[1]', label + '[2]' ];
+  }
+
+function expand_bz_and_j ( rows ) {
+  var out = [];
+  for ( var r in rows ) {
+    var row = rows[r];
+    var label;
+    var max_dist;
+    if ( row.type === 'bz' ) {
+      label = row.val;
+      max_dist = max_distance ( rows, r, label );
+      if ( max_dist < Math.pow ( 2, 7 ) ) {
+        out.push ( make_row ( 'cmd', [ 'bz_1', expand_label_1 ( label ) ] ) );
+        }
+      else if ( max_dist < Math.pow ( 2, 15 ) ) {
+        out.push ( make_row ( 'cmd', [ 'bz_2', expand_label_2 ( label ) ] ) );
+        }
+      else if ( max_dist < Math.pow ( 2, 23 ) ) {
+        out.push ( make_row ( 'cmd', [ 'bz_3', expand_label_3 ( label ) ] ) );
+        }
+      else {
+        out.push ( make_row ( 'cmd', [ 'bz_w', [ ] ] ) );
+        out.push ( make_row ( 'literal', [ 'label', label ] ) );
+        }
+      }
+    else if ( row.type === 'j' ) {
+      label = row.val;
+      max_dist = max_distance ( rows, r, label );
+      if ( max_dist < Math.pow ( 2, 7 ) ) {
+        out.push ( make_row ( 'cmd', [ 'j_1', expand_label_1 ( label ) ] ) );
+        }
+      else if ( max_dist < Math.pow ( 2, 15 ) ) {
+        out.push ( make_row ( 'cmd', [ 'j_2', expand_label_2 ( label ) ] ) );
+        }
+      else if ( max_dist < Math.pow ( 2, 23 ) ) {
+        out.push ( make_row ( 'cmd', [ 'j_3', expand_label_3 ( label ) ] ) );
+        }
+      else {
+        out.push ( make_row ( 'cmd', [ 'j_w', [ ] ] ) );
+        out.push ( make_row ( 'literal', [ 'label', label ] ) );
+        }
       }
     else {
       out.push ( row );
@@ -40,6 +144,7 @@ var root = {
     var data = obj.data;
 
     data = expand_li ( data );
+    data = expand_bz_and_j ( data );
 
     var patch = this.make_patch ( obj, data );
     this.patches.push ( patch );
