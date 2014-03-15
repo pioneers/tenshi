8cf40fc616335681c4e64d8a7bfc97cd7c8d5989
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-15 16:10:55 -0700

    Refactor require stub into one that can be shared
    Change-Id: I5fec5ffccbaf3d11b8688a8ad30f4ce59545b8db

--- angel-player/src/chrome/content/blockeditor/blockeditor-stub.js
+++
@@ -1,7 +0,0 @@
-function onLoad() {
-    var globalLoader = parent.document.tenshiGlobals.loader;
-    var module = loader.Module('tenshi/blockeditor/blockeditor',
-        'chrome://angel-player/content/blockeditor/blockeditor.js');
-    loader.load(globalLoader, module);
-    module.exports.onLoad(document);
-}
