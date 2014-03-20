34d4a8bf69f67bdf6960c63968b1bc84d299bff9
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-19 20:58:38 -0700

    Added serial port read timeout
    Change-Id: I5fcd3974fee05411c9edc73504f51ab2d3d9b42a

--- angel-player/src/chrome/content/ui2.js
+++ angel-player/src/chrome/content/ui2.js
@@ -5,14 +5,14 @@ var serport = require('tenshi/common/serport_linux');
 
 exports.init = function(_window) {
     serport.init();
-    var port = serport.SerialPortOpen("/dev/ttyUSB0", 115200);
-    var buf = new Uint8Array(256);
-    for (let i = 0; i < 256; i++) {
+    var port = serport.SerialPortOpen("/dev/ttyUSB0", 115200, 0.001);
+    var buf = new Uint8Array(128);
+    for (let i = 0; i < 128; i++) {
         buf[i] = i;
     }
     port.write(buf);
     var buf2 = port.read(256);
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < buf2.length; i++) {
         dump(buf2[i]);
         dump('\n');
     }
