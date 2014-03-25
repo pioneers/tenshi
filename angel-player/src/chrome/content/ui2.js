28e117caa9906b3487686ef06bcedc6a151a44ab
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-25 01:55:44 -0700

    Refactor serial port code to use web workers.
    The serial port code now works with Chrome Workers, which is basically
    idential to web workers except that it also has access to js-ctypes.
    Unfortunately, this code breaks support with being required via the
    normal Jetpack require system. However, this should be an acceptable
    loss because we will almost certainly eventually run this code in a
    worker so that it can run in the background while other parts of the
    app are running.
    
    Change-Id: I453bd0e47d3900b47648948590a1fcb61da1f1cc
    Reviewed-on: https://robertou.com/gerrit/335
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/ui2.js
+++ angel-player/src/chrome/content/ui2.js
@@ -1,20 +1,9 @@
 /* jshint globalstrict: true */
 "use strict";
 
-var serport = require('tenshi/common/serport_linux');
+const { ChromeWorker } = require('chrome');
+let serportWorker = null;
 
 exports.init = function(_window) {
-    serport.init();
-    var port = serport.SerialPortOpen("/dev/ttyUSB0", 115200, 0.001);
-    var buf = new Uint8Array(128);
-    for (let i = 0; i < 128; i++) {
-        buf[i] = i;
-    }
-    port.write(buf);
-    var buf2 = port.read(256);
-    for (let i = 0; i < buf2.length; i++) {
-        dump(buf2[i]);
-        dump('\n');
-    }
-    port.close();
+    serportWorker = new ChromeWorker("serportTest.js");
 };
