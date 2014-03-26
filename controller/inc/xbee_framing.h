7255f933392bd47062d50a1a43a4253c50bc5c1c
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-26 01:10:10 -0700

    Make XBee framing use Typpo
    wscript is really fragile. Kyle should fix this or something.
    
    Change-Id: I257363e9824e95aa9ce7c180628490d9440f0a86
    Reviewed-on: https://robertou.com/gerrit/345
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- controller/inc/xbee_framing.h
+++ controller/inc/xbee_framing.h
@@ -5,38 +5,7 @@
 
 #include "inc/uart_serial_driver.h"
 
-#define XBEE_MAGIC  0x7E
-
-#define XBEE_API_TYPE_TX64  0x00
-#define XBEE_API_TYPE_RX64  0x80
-
-typedef struct __attribute__((packed)) tag_xbee_tx64_header {
-  uint8_t xbee_api_type;
-  uint8_t frameId;
-  uint64_t xbee_dest_addr;
-  uint8_t options;
-  uint8_t data[];
-} xbee_tx64_header;
-
-typedef struct __attribute__((packed)) tag_xbee_rx64_header {
-  uint8_t xbee_api_type;
-  uint64_t xbee_src_addr;
-  uint8_t rssi;
-  uint8_t options;
-  uint8_t data[];
-} xbee_rx64_header;
-
-typedef struct __attribute__((packed)) tag_xbee_api_packet {
-  uint8_t xbee_api_magic;
-  // Note: big endian
-  uint16_t length;
-  union {
-    xbee_tx64_header tx64;
-    xbee_rx64_header rx64;
-    uint8_t xbee_api_type;
-    uint8_t payload[1];
-  };
-} xbee_api_packet;
+#include "xbee_typpo.h"   // NOLINT(build/include)
 
 extern ssize_t xbee_length_finder(uart_serial_module *module, uint8_t byte);
 extern int xbee_verify_checksum(xbee_api_packet *packet);
