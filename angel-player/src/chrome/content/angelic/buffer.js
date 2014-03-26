63476db8d9a2212d53e7800ef950e2b498c3dc13
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-03-26 00:44:59 -0700

    Add fs.js and buffer.js, which proxy to the appropriate implementations.
    Change-Id: Ib202bce3c9698c37c5aece2270ac0dd07295028f
    Reviewed-on: https://robertou.com/gerrit/351
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

---
+++ angel-player/src/chrome/content/angelic/buffer.js
@@ -0,0 +1,13 @@
+var misc = require ( './misc.js' );
+
+// Selects and proxies to the appropriate buffer implementation.
+
+if ( misc.environment === 'xulrunner' ) {
+  module.exports = require ( 'jetpack/sdk/io/buffer.js' );
+  }
+else if ( misc.environment === 'node' ) {
+  module.exports = require ( 'buffer' );
+  }
+else {
+  module.exports = require ( '../vendor-js/buffer.js' );
+  }
