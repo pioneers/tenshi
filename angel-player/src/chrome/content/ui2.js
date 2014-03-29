61e8bbbe741c4f0078ed64d6ff57c7ea52bca8fd
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-28 20:09:00 -0700

    Begin writing code to send packages to robot
    Begin coding some basic XBee interfacing code in JS utilizing Web Workers.
    Deletes useless rqou UI.
    
    Change-Id: I870b1a3f81ca22793b7129f561e40cba2c9149d2
    Reviewed-on: https://robertou.com/gerrit/361
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/ui2.js
+++
@@ -1,9 +0,0 @@
-/* jshint globalstrict: true */
-"use strict";
-
-const { ChromeWorker } = require('chrome');
-let serportWorker = null;
-
-exports.init = function(_window) {
-    serportWorker = new ChromeWorker("serportTest.js");
-};
