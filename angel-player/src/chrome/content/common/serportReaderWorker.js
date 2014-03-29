963e254336c7f6919a5e03a9c0fe268543e6c028
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-28 20:31:40 -0700

    Add XBee packet receive length handling
    Change-Id: Ie975181ef45a10c71f8949257b5f2bd0fc1cef0d
    Reviewed-on: https://robertou.com/gerrit/362
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/common/serportReaderWorker.js
+++ angel-player/src/chrome/content/common/serportReaderWorker.js
@@ -8,15 +8,44 @@ let serport = require('chrome://angel-player/content/common/serport_linux');
 serport.init();
 let serportObj = null;
 
+const PACKET_STATE_IDLE = 0;
+const PACKET_STATE_LEN = 1;
+let current_packet_state = PACKET_STATE_IDLE;
+
+// TODO(rqou): I don't want to import all of Typpo here
+const XBEE_MAGIC = 0x7E;
+
 // onmessage is magically called when a message is sent to this worker.
 onmessage = function(e) {
     // We assume we only ever get one message, the start message
     serportObj = serport.SerialPortClone(e.data.fd, e.data.timeout);
+    // TODO(rqou): Hardcoded for XBee packet structure; ugly.
+    let stubBuf = new Uint8Array(3);
+    let buf;
     while (true) {
-        // TODO(rqou): More intelligent reading/timeout-ing
-        let buf = serportObj.read(1);
-        if (buf.length == 1) {
-            postMessage(buf);
+        switch(current_packet_state) {
+        case PACKET_STATE_IDLE:
+            buf = serportObj.read(1);
+            if (buf[0] == XBEE_MAGIC) {
+                stubBuf[0] = buf[0];
+                current_packet_state = PACKET_STATE_LEN;
+            }
+            break;
+        case PACKET_STATE_LEN:
+            buf = serportObj.read(2);
+            let length = buf[0] << 8 | buf[1];
+            stubBuf[1] = buf[0];
+            stubBuf[2] = buf[1];
+
+            // TODO(rqou): Don't hardcode timeout?
+            serportObj.timeout = 0.1;
+            // Length does not include checksum -- add 1 byte
+            let mainData = serportObj.read(length + 1);
+            serportObj.timeout = null;
+            let combinedBuf = new Uint8Array(mainData.length + stubBuf.length);
+            combinedBuf.set(stubBuf, 0);
+            combinedBuf.set(mainData, stubBuf.length);
+            postMessage(combinedBuf);
         }
     }
 };
