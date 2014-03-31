4073db1d248a6cafd87ad48e1cf5dcf966030f53
Nikita Kitaev <nikitakitaev@pioneers.berkeley.edu>
2014-03-30 20:25:37 -0700

    Clean up controls
    Change-Id: I6a9512668e1bc3040dd46cb668eafb721706463d

--- angel-player/src/chrome/content/controls/main.js
+++ angel-player/src/chrome/content/controls/main.js
@@ -1,24 +1,109 @@
 /* jshint globalstrict: true */
 "use strict";
 
-let naive_packetizer = require('tenshi/common/naive_packetizer');
+let window,
+    $;
 
-let window = null;
-let document = null;
+let naive_packetizer = require('tenshi/common/naive_packetizer'),
+    texteditor = require('tenshi/texteditor/editor');
 
-exports.init = function(_window) {
-    window = _window;
-    document = window.document;
-    // TODO(rqou): This is a dumb way to expose this function
-    window.sendPacketizedData = naive_packetizer.sendPacketizedData;
-};
-
-let vm_generator = function () {return null;};
-
-exports.vm_generator = function () {
-    return vm_generator();
-};
+let current_vm = null,
+    vm_generator = function () {return null;};
 
 exports.set_vm_generator = function (x) {
     vm_generator = x;
 };
+
+exports.init = function(_window) {
+    window = _window;
+    $ = window.$;
+
+    $("#toggle").click(toggle_running);
+    $("#compile").click(compile_code);
+};
+
+function run() {
+    if (current_vm !== null) {
+        return; // Do not run multiple simultaneous vms
+    }
+
+    if (!texteditor.initialized()) {
+        $("#status").text("ERROR: text editor not yet loaded");
+        throw "ERROR: text editor not yet loaded";
+    }
+
+    var text = texteditor.get_text();
+    current_vm = vm_generator();
+
+    if (current_vm === null) {
+        $("#status").text("ERROR: no targets are open. Try opening Simulator or Console");
+        throw "ERROR: no vm could be created. Try opening a valid VM target page";
+    }
+
+    try {
+        current_vm.load_text(text);
+        current_vm.start_main();
+    } catch(err) {
+        var console = require("tenshi/console/console");
+        if (!console.initialized()) {
+            return;
+        }
+        console.report_error(err);
+        throw err;
+    }
+    current_vm.set_evaluation_rate(100, 0.01);
+}
+
+function stop() {
+    if (current_vm === null) {
+        return; // No vm is running
+    }
+
+    // These arguments are special-cased to fully stop the VM
+    current_vm.set_evaluation_rate(0, 0);
+    current_vm = null;
+}
+
+function toggle_running() {
+    if (current_vm === null) {
+        run();
+        $("#toggle").text("STOP");
+        $("#status").text("Running in: " + current_vm.source);
+    } else {
+        stop();
+        $("#toggle").text("RUN");
+        $("#status").text("");
+    }
+}
+
+function compile_code() {
+    if (current_vm !== null) {
+        return; // Do not run multiple simultaneous vms
+    }
+
+    if (!texteditor.initialized) {
+        $("#status").text("ERROR: text editor not yet loaded");
+        throw "ERROR: text editor not yet loaded";
+    }
+
+    var text = texteditor.get_text();
+    current_vm = vm_generator();
+
+    if (current_vm === null) {
+        $("#status").text("ERROR: no targets are open. Try opening Simulator or Console");
+        throw "ERROR: no vm could be created. Try opening a valid VM target page";
+    }
+
+    try {
+        current_vm.load_text(text);
+        var pkg = current_vm.get_pkg('ARM');
+        naive_packetizer.sendPacketizedData(pkg);
+    } catch(err) {
+        var console = require("tenshi/console/console");
+        if (!console.initialized()) {
+            return;
+        }
+        console.report_error(err);
+        throw err;
+    }
+}
