28cc49491f09ffdfdc6c6c778a08ece37e4ec275
Robert Ou <robertou@pioneers.berkeley.edu>
2014-03-31 01:55:42 -0700

    Initial implementation of bulk transfer protocol on embedded
    This will eventually be refactored to not be one giant clusterfuck
    of a function. Instead, there will be separate logic for a) harassing
    the host, b) receiving data, and c) sending telementry.
    
    Change-Id: Id152ffbeb8219fa792bc600fb66313b448b01f20
    Reviewed-on: https://robertou.com/gerrit/382
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- controller/src/main.c
+++ controller/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "inc/FreeRTOS.h"
 #include "inc/button_driver.h"
@@ -14,151 +15,148 @@
 
 #include "legacy_piemos_framing.h"   // NOLINT(build/include)
 
-static portTASK_FUNCTION_PROTO(blinkTask, pvParameters) {
-  uint8_t buf[64];
+uint8_t *code_buffer;
 
-  uint8_t counter = 0;
+// TODO(rqou): Move this elsewhere
+// TODO(rqou): This entire function is a hack
+static portTASK_FUNCTION_PROTO(radioTask, pvParameters) {
+  // TODO(rqou): More intelligent interleaved
+  uint32_t code_received_to = 0;
+  uint32_t code_received_len = 0;
+  int got_a_packet = 0;
+  const uint32_t CHUNK_SIZE = 64;
+  // TODO(rqou): Hack
+  uint8_t txbuf[64];
+  // TODO(rqou): Ugly uglly
+  uint64_t host_addr = 0;
 
   while (1) {
+    uint8_t *buf;
     size_t len;
-    uint8_t *buf2;
-    void *txn;
-    // Blink green LED
-    // GPIO_BANK(PINDEF_GREEN_LED)->ODR ^=
-    //   (1 << GPIO_PIN(PINDEF_GREEN_LED));
 
-    // debug_uart_tx("Hello World!\r\n", 14);
+    if (!got_a_packet) {
+      // Harass host
+      xbee_api_packet *packetOut = (xbee_api_packet *)(txbuf);
+      packetOut->xbee_api_magic = XBEE_MAGIC;
+      int payloadLen = sizeof(xbee_tx64_header) + sizeof(tenshi_bulk_chunkreq);
+      packetOut->length = __REV16(payloadLen);
+      packetOut->payload.tx64.xbee_api_type = XBEE_API_TYPE_TX64;
+      packetOut->payload.tx64.frameId = 0;
+      packetOut->payload.tx64.xbee_dest_addr = host_addr;
+      packetOut->payload.tx64.options = 0;
+      tenshi_bulk_chunkreq *bulk_chunkreq =
+        (tenshi_bulk_chunkreq *)(packetOut->payload.tx64.data);
+      bulk_chunkreq->ident = TENSHI_NAIVE_BULK_CHUNKREQ_IDENT;
+      bulk_chunkreq->stream_id = 0;
+      bulk_chunkreq->start_addr = code_received_to;
+      if (code_received_to + CHUNK_SIZE > code_received_len) {
+        bulk_chunkreq->end_addr = code_received_len;
+      } else {
+        bulk_chunkreq->end_addr = code_received_to + CHUNK_SIZE;
+      }
+      xbee_fill_checksum(packetOut);
 
-    // vTaskDelay(1000);
+      // TODO(rqou): Asynchronous?
+      // TODO(rqou): Abstract away the +4 properly
+      // TODO(rqou): Error handling
+      void *txn = uart_serial_send_data(radio_driver, buf, payloadLen + 4);
+      while ((uart_serial_send_status(radio_driver, txn) !=
+          UART_SERIAL_SEND_DONE) &&
+          (uart_serial_send_status(radio_driver, txn) !=
+            UART_SERIAL_SEND_ERROR)) {}
+      uart_serial_send_finish(radio_driver, txn);
+    }
+    got_a_packet = 0;
 
-    /*if (button_driver_get_button_state(0) ||
-        button_driver_get_button_state(1)) {
-      led_driver_set_mode(PATTERN_BACK_AND_FORTH);
-    } else {
-      led_driver_set_mode(PATTERN_DEFAULT_CHASER);
-    }*/
-
-    /*void *txn = i2c_issue_transaction(i2c1_driver, 0x1c, "\x60", 1, buf, 32);
-    while ((i2c_transaction_status(i2c1_driver, txn) !=
-        I2C_TRANSACTION_STATUS_DONE) &&
-        (i2c_transaction_status(i2c1_driver, txn) !=
-          I2C_TRANSACTION_STATUS_ERROR)) {}
-    i2c_transaction_finish(i2c1_driver, txn);*/
-
-    /*
-    txn = uart_serial_send_data(smartsensor_1, "\x00\x0A""CDEFGHIJKL",
-      12);
-    while ((uart_serial_send_status(smartsensor_1, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(smartsensor_1, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(smartsensor_1, txn);
-
-    buf2 = uart_serial_receive_packet(smartsensor_1, &len, 1);
-    vPortFree(buf2);
-
-    txn = uart_serial_send_data(smartsensor_2, "\x00\x0A""CDEFGHIJKL",
-      12);
-    while ((uart_serial_send_status(smartsensor_2, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(smartsensor_2, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(smartsensor_2, txn);
-
-    buf2 = uart_serial_receive_packet(smartsensor_2, &len, 1);
-    vPortFree(buf2);
-
-    txn = uart_serial_send_data(smartsensor_3, "\x00\x0A""CDEFGHIJKL",
-      12);
-    while ((uart_serial_send_status(smartsensor_3, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(smartsensor_3, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(smartsensor_3, txn);
-
-    buf2 = uart_serial_receive_packet(smartsensor_3, &len, 1);
-    vPortFree(buf2);
-
-    txn = uart_serial_send_data(smartsensor_4, "\x00\x0A""CDEFGHIJKL",
-      12);
-    while ((uart_serial_send_status(smartsensor_4, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(smartsensor_4, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(smartsensor_4, txn);
-
-    buf2 = uart_serial_receive_packet(smartsensor_4, &len, 1);
-    vPortFree(buf2);
-
-    txn = uart_serial_send_data(radio_driver, "\x00\x0A""CDEFGHIJKL",
-      12);
-    while ((uart_serial_send_status(radio_driver, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(radio_driver, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(radio_driver, txn);
-
-    buf2 = uart_serial_receive_packet(radio_driver, &len, 1);
-    vPortFree(buf2);
-
-    vTaskDelay(500);
-    */
-
-    buf2 = uart_serial_receive_packet(radio_driver, &len, 1);
-    if (!xbee_verify_checksum(buf2)) {
+    buf = uart_serial_receive_packet(radio_driver, &len, 0);
+    if (!buf) {
       continue;
     }
-    xbee_api_packet *packetIn = buf2;
+    if (!xbee_verify_checksum(buf)) {
+      vPortFree(buf);
+      continue;
+    }
+    xbee_api_packet *packetIn = (xbee_api_packet *)buf;
     if (packetIn->payload.xbee_api_type != XBEE_API_TYPE_RX64) {
+      vPortFree(buf);
       continue;
     }
-    xbee_api_packet *packetOut = buf;
-    packetOut->xbee_api_magic = XBEE_MAGIC;
-    int lenThing = sizeof(xbee_tx64_header) + sizeof(pier_outgoingdata);
-    packetOut->length = __REV16(lenThing);
-    packetOut->payload.tx64.xbee_api_type = XBEE_API_TYPE_TX64;
-    packetOut->payload.tx64.frameId = 0;
-    packetOut->payload.tx64.xbee_dest_addr =
-      packetIn->payload.rx64.xbee_src_addr;
-    packetOut->payload.tx64.options = 0;
-    pier_incomingdata *inData = &(packetIn->payload.rx64.data);
-    pier_outgoingdata *outData = &(packetOut->payload.tx64.data);
+    // ident byte for PiEMOS framing
+    switch (packetIn->payload.rx64.data[0]) {
+    case PIER_INCOMINGDATA_IDENT:
+      // TODO(rqou): do something useful!
+      break;
 
-    if (inData->ident != PIER_INCOMINGDATA_IDENT) {
-      continue;
+    // Naive bulk protocol
+    case TENSHI_NAIVE_BULK_START_IDENT:
+      {
+        tenshi_bulk_start *bulk_start =
+          (tenshi_bulk_start *)(packetIn->payload.rx64.data);
+        // TODO(rqou): What happens if I already have one?
+        // TODO(rqou): Stream ID?
+        code_buffer = pvPortMalloc(bulk_start->length);
+        code_received_to = 0;
+        code_received_len = bulk_start->length;
+        got_a_packet = 1;
+
+        // TODO(rqou): Refactor this logic
+        host_addr = packetIn->payload.rx64.xbee_src_addr;
+      }
+      break;
+    case TENSHI_NAIVE_BULK_CHUNK_IDENT:
+      {
+        tenshi_bulk_chunk *bulk_chunk =
+          (tenshi_bulk_chunk *)(packetIn->payload.rx64.data);
+        memcpy(code_buffer + bulk_chunk->start_addr, bulk_chunk->data,
+          bulk_chunk->end_addr - bulk_chunk->start_addr);
+        // TODO(rqou): Properly handle out-of-order
+        code_received_to = bulk_chunk->end_addr;
+        got_a_packet = 1;
+
+        if (code_received_to == code_received_len) {
+          xbee_api_packet *packetOut = (xbee_api_packet *)(txbuf);
+          packetOut->xbee_api_magic = XBEE_MAGIC;
+          int payloadLen = sizeof(xbee_tx64_header) + sizeof(tenshi_bulk_stop);
+          packetOut->length = __REV16(payloadLen);
+          packetOut->payload.tx64.xbee_api_type = XBEE_API_TYPE_TX64;
+          packetOut->payload.tx64.frameId = 0;
+          packetOut->payload.tx64.xbee_dest_addr =
+            packetIn->payload.rx64.xbee_src_addr;
+          packetOut->payload.tx64.options = 0;
+          tenshi_bulk_stop *bulk_stop =
+            (tenshi_bulk_stop *)(packetOut->payload.tx64.data);
+          bulk_stop->ident = TENSHI_NAIVE_BULK_STOP_IDENT;
+          bulk_stop->stream_id = 0;
+          xbee_fill_checksum(packetOut);
+
+          // TODO(rqou): Asynchronous?
+          // TODO(rqou): Abstract away the +4 properly
+          // TODO(rqou): Error handling
+          void *txn = uart_serial_send_data(radio_driver, buf, payloadLen + 4);
+          while ((uart_serial_send_status(radio_driver, txn) !=
+              UART_SERIAL_SEND_DONE) &&
+              (uart_serial_send_status(radio_driver, txn) !=
+                UART_SERIAL_SEND_ERROR)) {}
+          uart_serial_send_finish(radio_driver, txn);
+        }
+      }
+      break;
+
+    default:
+      // TODO(rqou): Report error or something?
+      break;
     }
 
-    outData->ident = PIER_OUTGOINGDATA_IDENT;
-    outData->analog[0] = inData->analog[0] + inData->analog[1];
-    outData->analog[1] = counter++;
+    vPortFree(buf);
 
-    xbee_fill_checksum(packetOut);
-
-    vPortFree(buf2);
-    txn = uart_serial_send_data(radio_driver, buf, lenThing + 4);
-    while ((uart_serial_send_status(radio_driver, txn) !=
-        UART_SERIAL_SEND_DONE) &&
-        (uart_serial_send_status(radio_driver, txn) !=
-          UART_SERIAL_SEND_ERROR)) {}
-    uart_serial_send_finish(radio_driver, txn);
+    // TODO(rqou): Proper timer
+    vTaskDelay(20 / portTICK_RATE_MS);
   }
 }
 
 int main(int argc, char **argv) {
   led_driver_init();
   button_driver_init();
-  // Enable GPIOD clock
-  // RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-  // Enable GPIOB clock
-  // RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-  // TODO(rqou): Need some kind of io_init function
-
-  // Set red and green LED GPIO output
-  // CONFIGURE_IO(RED_LED);
-  // CONFIGURE_IO(GREEN_LED);
-  // Turn on red LED
-  // GPIO_BANK(PINDEF_RED_LED)->ODR =
-  //   (1 << GPIO_PIN(PINDEF_RED_LED));
 
   // debug_uart_setup();
 
@@ -174,6 +172,6 @@ int main(int argc, char **argv) {
   // Setup radio
   radio_driver_init();
 
-  xTaskCreate(blinkTask, "Blink", 256, NULL, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(radioTask, "Radio", 256, NULL, tskIDLE_PRIORITY, NULL);
   vTaskStartScheduler();
 }
