8f2924d30af58c6fe2d162e2ffcdbac99c54135c
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-03-30 22:30:53 -0700

    Implemented factory.get_size for structs and unions.
    Change-Id: I1ce2c3904e9808e0ab3adcdfe3e0773cabeb6225

---
+++ angel-player/src/chrome/content/angelic/tests/factory/size_ngl_buffer.js
@@ -0,0 +1,12 @@
+var root = process.argv[2];
+
+var buffer = require ( root + '/buffer.js' );
+var misc = require ( root + '/misc.js' );
+var factory = require ( root + '/factory.js' );
+
+var fact = factory.make ( );
+fact.set_target_type ( 'ARM' );
+fact.load_type_file ( root + '/common_defs/ngl_types.yaml' );
+
+misc.assert ( fact.get_size ( 'ngl_buffer' ) === 4 * ( 2 + 1 ),
+              'Size of ngl_buffer should be 12 bytes.' );
