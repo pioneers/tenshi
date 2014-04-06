45da6daa8a27bb917c37972102d819beef6716e3
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-04-06 14:32:56 -0700

    Added tests used for fixing functions in ngl_vm.
    Change-Id: Ib2bf922fb0f553aeab8df0a56512c54b2029381b

--- angel-player/src/chrome/content/angelic/tests/ngl_vm/simple.js
+++ angel-player/src/chrome/content/angelic/tests/ngl_vm/simple.js
@@ -8,26 +8,14 @@ function compile_and_run ( text ) {
     vm.make_exfn ( 0, 'print_float', misc.print ),
     ] );
   vm.load_text ( text );
-  vm.save_pkg ( 'x86_64', 'fib.ngl_pkg', misc.print );
+  vm.save_pkg ( 'x86_64', 'simple.ngl_pkg', misc.print );
   return vm;
   }
-
 var code = '' +
-'fib = fn (n):\n' +
-'    n = n - 1\n' +
-'    a = 0\n' +
-'    b = 1\n' +
-'    while n != 0:\n' +
-'        temp = a + b\n' +
-'        a = b\n' +
-'        b = temp\n' +
-'        n = n - 1\n' +
-'    return b\n' +
+'func = fn (n):\n' +
+'    return 255 + n\n' +
 'main = fn:\n' +
-'    x = 50\n' +
-'    print_float (x)\n' +
-'    fib50 = fib(x)\n' +
-'    print_float (fib50)\n' +
+'    print_float(func(42))\n' +
 '';
 
 compile_and_run ( code );
