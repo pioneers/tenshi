d8e85c17163e38c9cfc985598fbacd1cf91499c6
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-04-09 22:40:02 -0700

    Fixed returns from external functions in ngl_vm.
    Change-Id: I11156c4bbe801fa06ca4ab8d145cbd9d6aee5f24

--- controller/vm/src/ngl_call_define.c
+++ controller/vm/src/ngl_call_define.c
@@ -35,7 +35,7 @@
 #define NGL_ARG_PTR(idx, type) ((type) arg[(idx)].pointer)
 
 #define NGL_RET(type) (NGL_RET_IDX(0, type))
-#define NGL_RET_IDX(idx, type) ((type *) &arg[(idx)])
+#define NGL_RET_IDX(idx, type) ((type *) &arg[(idx) + (ngl_call_argc)])
 
 #define NGL_ERROR() (&error)
 
