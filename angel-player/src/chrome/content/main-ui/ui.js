0348fa30a8b32a2ed38b931f9fcfc2f717dedb28
Robert Ou <robertou@pioneers.berkeley.edu>
2014-04-12 20:32:35 -0700

    Add basic layouts for various sidebars in different environments
    Change-Id: I60109d89ae45d8e2484bb77212ac9dded853505d
    Reviewed-on: https://robertou.com/gerrit/461
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/main-ui/ui.js
+++ angel-player/src/chrome/content/main-ui/ui.js
@@ -1,3 +1,6 @@
+/* jshint globalstrict: true */
+"use strict";
+
 let window;
 let document;
 let $;
@@ -7,22 +10,59 @@ let currentSelectedTab = -1;
 const XUL_XMLNS =
     'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
 
+// Different ways to arrange the divs to suit different environments. Contains
+// relative flexbox sizes for each element.
+const LAYOUTS = {
+    "center-with-all-sidebars": {
+        top: "1",
+        centerGroup: "8",
+        bottom: "1",
+        left: "1",
+        centerPane: "8",
+        right: "1",
+    },
+    "center-only": {
+        top: "0",
+        centerGroup: "1",
+        bottom: "0",
+        left: "0",
+        centerPane: "1",
+        right: "0",
+    }
+};
+
 // Different "environments" such as coding, driving, home page, etc.
 const ENVIRONMENTS = [
     {
         text: "Home",
         image: 'chrome://angel-player/content/main-ui/assets/home.png',
-        activate: function() {dump('asdf1\n');},
-        deactivate: function() {dump('asdf2\n');},
+        activate: function() {
+            switchLayout("center-with-all-sidebars");
+        },
+        deactivate: function() {},
     }, {
         text: "Code",
         image: 'chrome://angel-player/content/main-ui/assets/code.png',
-        activate: function() {dump('asdf3\n');},
-        deactivate: function() {dump('asdf4\n');},
+        activate: function() {
+            switchLayout("center-only");
+        },
+        deactivate: function() {},
     }
 ];
 
+function switchLayout(newLayout) {
+    let layout = LAYOUTS[newLayout];
+    $("#topDiv").css('flex', layout.top);
+    $("#mainCenter").css('flex', layout.centerGroup);
+    $("#bottomDiv").css('flex', layout.bottom);
+    $("#leftDiv").css('flex', layout.left);
+    $("#centerDiv").css('flex', layout.centerPane);
+    $("#rightDiv").css('flex', layout.right);
+}
+
 function newTabSelected() {
+    /* jshint validthis: true */
+
     if (currentSelectedTab != -1) {
         ENVIRONMENTS[currentSelectedTab].deactivate();
     }
