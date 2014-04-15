9a1c8c90d70c9341e1a9600f734c20dcefde6953
Robert Ou <robertou@pioneers.berkeley.edu>
2014-04-15 02:35:35 -0700

    Added ability to save text editor text
    Change-Id: I2077c183f616908da7ccfba81c09db4b5f1f8df1
    Reviewed-on: https://robertou.com/gerrit/469
    Tested-by: Jenkins CI
    Reviewed-by: Kyle Zentner <kylezentner@pioneers.berkeley.edu>

--- angel-player/src/chrome/content/common/robot_application.js
+++ angel-player/src/chrome/content/common/robot_application.js
@@ -23,6 +23,13 @@ exports.CreateEmptyRobotApplication = function() {
     let ret = {};
 
     ret.name = "New Robot Application";
+    // TODO(rqou): Remove this
+    ret.text_code = "main = fn:\n" +
+                    "    while 0!=1:\n" +
+                    "        x = get_sensor(1)\n" +
+                    "        y = get_sensor(3)\n" +
+                    "        set_motor(0, x)\n" +
+                    "        set_motor(1, y)\n";
 
     setExports(ret);
     return ret;
