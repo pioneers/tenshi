dba23c960be63ac2fcac92ad652b694a152cc431
Robert Ou <robertou@pioneers.berkeley.edu>
2014-04-15 02:35:35 -0700

    Added ability to save text editor text
    Change-Id: I2077c183f616908da7ccfba81c09db4b5f1f8df1

--- angel-player/src/chrome/content/texteditor/editor.js
+++ angel-player/src/chrome/content/texteditor/editor.js
@@ -4,6 +4,27 @@ var window,
 var editor,
     initialized = false;
 
+let lastEditorChangeTimeout = -1;
+
+const global_state = require('tenshi/common/global_state');
+const robot_application = require('tenshi/common/robot_application');
+
+function saveEditorText() {
+    let robotApp = global_state.get('robot_application');
+    robotApp.text_code = editor.getValue();
+    dump(robotApp.text_code);
+}
+
+function onEditorChanged(e) {
+    // TODO(rqou): Is this inefficient?
+    if (lastEditorChangeTimeout != -1) {
+        window.clearTimeout(lastEditorChangeTimeout);
+    }
+
+    // After no typing for 0.5 second, save the text.
+    lastEditorChangeTimeout = window.setTimeout(saveEditorText, 500);
+}
+
 exports.get_text = function() {
   return editor.getValue();
 };
@@ -17,5 +38,13 @@ exports.init = function(_window) {
   ace = window.ace;
 
   editor = ace.edit("editor");
+
+  let robotApp = global_state.get('robot_application');
+  editor.setValue(robotApp.text_code);
+  // HACK: for some reason, setting the value selects text
+  editor.getSelection().clearSelection();
+
+  editor.getSession().on('change', onEditorChanged);
+
   initialized = true;
 };
