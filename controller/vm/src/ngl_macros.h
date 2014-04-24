87e496d0ed464a2755f3c53ee1c0694599019afc
Kyle Zentner <kylezentner@pioneers.berkeley.edu>
2014-04-23 17:13:31 -0700

    Decreased ngl_type overhead.
    Change-Id: I101d78868e50ca801a65bb5665103e2dac2971b3
    Reviewed-on: https://robertou.com/gerrit/490
    Tested-by: Jenkins CI
    Reviewed-by: Robert Ou <robertou@pioneers.berkeley.edu>

--- controller/vm/src/ngl_macros.h
+++ controller/vm/src/ngl_macros.h
@@ -34,36 +34,19 @@
 
 #define ngl_type_to_ngl_type(type) (join_token(ngl_type_, type))
 
-#define ngl_check_static_test(type) (join_token(ngl_check_, type))
-
-#define ngl_check_to_ngl_val_pointer(type, value) \
-  ((ngl_val) { .pointer =(ngl_obj *) ngl_check_static_test(type)(value) })
-#define ngl_check_to_ngl_val_integer(type, value) \
-  ((ngl_val) { .integer = ngl_check_static_test(type)(value) })
-#define ngl_check_to_ngl_val_uinteger(type, value) \
-  ((ngl_val) { .uinteger = ngl_check_static_test(type)(value) })
-#define ngl_check_to_ngl_val_floating(type, value) \
-  ((ngl_val) { .floating = ngl_check_static_test(type)(value) })
-
-
 #define ngl_declare_alien(alien_type) \
   extern ngl_type * join_token(ngl_type_, alien_type); \
-  alien_type ngl_check_static_test(alien_type) (alien_type to_check); \
   ngl_declare_ptr(alien_type)
 
 #define ngl_define_alien(alien_type) \
-  alien_type ngl_check_static_test(alien_type) (alien_type to_check) { \
-    return to_check; \
-    } \
   ngl_type * join_token(ngl_type_, alien_type); \
+  ngl_type_alien join_token3(ngl_type_, alien_type, _inst); \
+  extern ngl_type_base join_token3(ngl_type_, base_type, _inst); \
   ngl_define_ptr(alien_type)
 
 #define ngl_init_alien(alien_type) \
   join_token(ngl_type_, alien_type) = \
-      (ngl_type *) ngl_alloc_simple(ngl_type_alien, 1); \
-  if (join_token(ngl_type_, alien_type) == NULL) { \
-    ngl_alloc_error(); \
-    } \
+      (ngl_type *) &join_token3(ngl_type_, alien_type, _inst); \
   join_token(ngl_type_, alien_type)->header.type = ngl_type_ngl_type_alien; \
   join_token(ngl_type_, alien_type)->name = \
       ngl_str_lit(string_of_macro(alien_type)); \
@@ -75,22 +58,17 @@
 
 #define ngl_declare_base(base_type) \
   extern ngl_type * join_token(ngl_type_, base_type); \
-  base_type ngl_check_static_test(base_type) (base_type to_check); \
+  extern ngl_type_base join_token3(ngl_type_, base_type, _inst); \
   ngl_declare_ptr(base_type)
 
 #define ngl_define_base(base_type) \
   ngl_type * join_token(ngl_type_, base_type); \
-  base_type ngl_check_static_test(base_type) (base_type to_check) { \
-    return to_check; \
-    } \
+  ngl_type_base join_token3(ngl_type_, base_type, _inst); \
   ngl_define_ptr(base_type)
 
 #define ngl_init_base(base_type) \
   join_token(ngl_type_, base_type) = \
-      (ngl_type *) ngl_alloc_simple(ngl_type_base, 1); \
-  if (join_token(ngl_type_, base_type) == NULL) { \
-    ngl_alloc_error(); \
-    } \
+      (ngl_type *) &join_token3(ngl_type_, base_type, _inst); \
   join_token(ngl_type_, base_type)->header.type = ngl_type_ngl_type_base; \
   join_token(ngl_type_, base_type)->name = \
       ngl_str_lit(string_of_macro(base_type)); \
@@ -102,24 +80,17 @@
 
 #define ngl_declare_composite(composite_type) \
   extern ngl_type * join_token(ngl_type_, composite_type); \
-  composite_type ngl_check_static_test(composite_type) \
-      (composite_type to_check); \
+  ngl_type_composite join_token3(ngl_type_, composite_type, _inst); \
   ngl_declare_ptr(composite_type)
 
 #define ngl_define_composite(composite_type) \
   ngl_type * join_token(ngl_type_, composite_type); \
-  composite_type ngl_check_static_test(composite_type) \
-      (composite_type to_check) { \
-    return to_check; \
-    } \
+  ngl_type_composite join_token3(ngl_type_, composite_type, _inst); \
   ngl_define_ptr(composite_type)
 
 #define ngl_init_composite(composite_type) \
   join_token(ngl_type_, composite_type) = \
-      (ngl_type *) ngl_alloc_simple(ngl_type_composite, 1); \
-  if (join_token(ngl_type_, composite_type) == NULL) { \
-    ngl_alloc_error(); \
-    } \
+      (ngl_type *) &join_token3(ngl_type_, composite_type, _inst); \
   join_token(ngl_type_, composite_type)->header.type = \
       ngl_type_ngl_type_composite; \
   join_token(ngl_type_, composite_type)->name = \
@@ -153,30 +124,15 @@
 #define ngl_declare_ptr(base_type) \
   typedef base_type * join_token(base_type, _ptr); \
   extern ngl_type * join_token3(ngl_type_, base_type, _ptr); \
-  base_type * ngl_check_static_test(join_token(base_type, _ptr))(base_type *); \
-  typedef base_type ** join_token(base_type, _ptr_ptr); \
-  extern ngl_type * join_token3(ngl_type_, base_type, _ptr_ptr); \
-  base_type ** ngl_check_static_test \
-      (join_token(base_type, _ptr_ptr))(base_type **)
+  extern ngl_type_pointer join_token3(ngl_type_, base_type, _ptr_inst);
 
 #define ngl_define_ptr(base_type) \
   ngl_type * join_token3(ngl_type_, base_type, _ptr); \
-  base_type * ngl_check_static_test(join_token(base_type, _ptr)) \
-      (base_type * to_check) { \
-    return to_check; \
-    } \
-  base_type ** ngl_check_static_test(join_token(base_type, _ptr_ptr)) \
-      (base_type ** to_check) { \
-    return to_check; \
-    } \
-  ngl_type * join_token3(ngl_type_, base_type, _ptr_ptr)
+  ngl_type_pointer join_token3(ngl_type_, base_type, _ptr_inst);
 
 #define ngl_init_ptr(base_type) \
   join_token3(ngl_type_, base_type, _ptr) = \
-      (ngl_type *) ngl_alloc_simple(ngl_type_pointer, 1); \
-  if (join_token3(ngl_type_, base_type, _ptr) == NULL) { \
-    ngl_alloc_error(); \
-    } \
+      (ngl_type *) &join_token3(ngl_type_, base_type, _ptr_inst); \
   join_token3(ngl_type_, base_type, _ptr)->header.type = \
       ngl_type_ngl_type_pointer; \
   join_token3(ngl_type_, base_type, _ptr)->name = \
@@ -185,23 +141,10 @@
       sizeof(base_type *); \
   ((ngl_type_pointer *) join_token3(ngl_type_, base_type, _ptr))->subtype = \
         ngl_type_to_ngl_type(base_type); \
-  join_token3(ngl_type_, base_type, _ptr_ptr) = \
-        (ngl_type *) ngl_alloc_simple(ngl_type_pointer, 1); \
   if (join_token(ngl_type_, base_type) == NULL) { \
     ngl_alloc_error(); \
     } \
-  join_token3(ngl_type_, base_type, _ptr_ptr)->header.type = \
-      ngl_type_ngl_type_pointer; \
-  join_token3(ngl_type_, base_type, _ptr_ptr)->ptr_of = NULL; \
-  join_token3(ngl_type_, base_type, _ptr_ptr)->name = \
-      ngl_str_lit(string_of_macro(join_token(base_type, _ptr_ptr))); \
-  join_token3(ngl_type_, base_type, _ptr_ptr)->size = \
-      sizeof(base_type **); \
-((ngl_type_pointer *) join_token3(ngl_type_, base_type, \
-      _ptr_ptr))->subtype = \
-      ngl_type_to_ngl_type(join_token(base_type, _ptr)); \
-  join_token3(ngl_type_, base_type, _ptr)->ptr_of = \
-      join_token3(ngl_type_, base_type, _ptr_ptr);
+  join_token3(ngl_type_, base_type, _ptr)->ptr_of = NULL;
 
 #define ngl_obj_cast(target_type, value) \
 (value &&(value)->header.type == ngl_type_to_ngl_type(target_type) ? \
