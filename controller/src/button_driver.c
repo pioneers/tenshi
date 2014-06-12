942052d98bff5eb9faf3c383acceda86f454a085
Casey Duckering <cduckering@gmail.com>
2014-06-11 22:31:45 -0700

    Fix Robert's button debounce code.

--- controller/src/button_driver.c
+++ controller/src/button_driver.c
@@ -9,7 +9,16 @@
 static volatile int button0_state = 0;
 static volatile int button1_state = 0;
 
+static volatile int button0_actual_state = 0;
+static volatile int button1_actual_state = 0;
+
+static volatile portTickType button0_last_toggle = 0;
+static volatile portTickType button1_last_toggle = 0;
+
 #define DEBOUNCE_DELAY  (50 / portTICK_RATE_MS)
+
+void button_driver_update_debounce();
+
 
 void button_driver_init(void) {
   // Enable SYSCFG clock so that EXTI can be set
@@ -45,6 +54,8 @@ void button_driver_init(void) {
 }
 
 int button_driver_get_button_state(int button) {
+  button_driver_update_debounce();
+
   if (button == 0) {
     return button0_state;
   } else if (button == 1) {
@@ -55,37 +66,49 @@ int button_driver_get_button_state(int button) {
   }
 }
 
+void button_driver_update_debounce() {
+  portTickType time_now = xTaskGetTickCountFromISR();
+  if (time_now - button0_last_toggle >= DEBOUNCE_DELAY) {
+    button0_state = button0_actual_state;
+  }
+  if (time_now - button1_last_toggle >= DEBOUNCE_DELAY) {
+    button1_state = button1_actual_state;
+  }
+}
+
 void EXTI15_10_IRQHandler(void) {
   // TODO(rqou): How to share this vector (or if it is even necessary)
-
-  static portTickType button0_last_toggle = 0;
-  static portTickType button1_last_toggle = 0;
 
   portTickType time_now = xTaskGetTickCountFromISR();
 
   if (EXTI->PR & (1 << GPIO_PIN(PINDEF_BUTTON0))) {
     // Clear pending
     EXTI->PR = (1 << GPIO_PIN(PINDEF_BUTTON0));
+    // Invert is necessary because pin idles at 1
+    int newState =
+      !(GPIO_BANK(PINDEF_BUTTON0)->IDR & (1 << GPIO_PIN(PINDEF_BUTTON0)));
 
     if (time_now - button0_last_toggle >= DEBOUNCE_DELAY) {
-      // Invert is necessary because pin idles at 1
-      button0_state =
-        !(GPIO_BANK(PINDEF_BUTTON0)->IDR & (1 << GPIO_PIN(PINDEF_BUTTON0)));
+      button0_state = newState;
     }
 
     button0_last_toggle = time_now;
+    button0_actual_state = newState;
   }
 
   if (EXTI->PR & (1 << GPIO_PIN(PINDEF_BUTTON1))) {
     // Clear pending
     EXTI->PR = (1 << GPIO_PIN(PINDEF_BUTTON1));
+    // Invert is necessary because pin idles at 1
+    int newState =
+      !(GPIO_BANK(PINDEF_BUTTON1)->IDR & (1 << GPIO_PIN(PINDEF_BUTTON1)));
 
     if (time_now - button1_last_toggle >= DEBOUNCE_DELAY) {
       // Invert is necessary because pin idles at 1
-      button1_state =
-        !(GPIO_BANK(PINDEF_BUTTON1)->IDR & (1 << GPIO_PIN(PINDEF_BUTTON1)));
+      button1_state = newState;
     }
 
     button1_last_toggle = time_now;
+    button1_actual_state = newState;
   }
 }
