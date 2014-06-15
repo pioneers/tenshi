0b75d22ddf57c36d06f6d860c5d8e631403e2f4a
Casey Duckering <cduckering@gmail.com>
2014-06-14 18:32:56 -0700

    Scripts for configuring avrdude and flashing and setting fuses on smart sensors.

---
+++ tools/flash_smart_sensor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname `which $0`)
+cd $SCRIPT_PATH
+
+if diff /etc/avrdude.conf avrdude.conf >/dev/null ; then
+  echo
+else
+  echo "$(tput setaf 1)Warning: Your /etc/avrdude.conf file may not be correct.
+Press Ctrl+C and then run ./tools/setup_avrdude.sh to install the correct file
+or press [Enter] to continue anyways.$(tput sgr 0)"
+  read
+fi
+
+set -x #echo on
+sudo avrdude -c usbtiny -p attiny1634 -U flash:w:../build/artifacts/smartsensor_fw/opt/smartsensor_fw.hex:i
