8eca6f6c4bf1f7d318a5c04d31d751cb20f0bd1f
Tobin Holcomb <Tobinholcomb@pioneers.berkeley.edu>
2014-06-20 21:05:14 -0700

    Set up receiving of proper package headers

--- smartsensor_fw/src/main.c
+++ smartsensor_fw/src/main.c
@@ -16,13 +16,12 @@
 // under the License
 
 #define F_CPU 8000000UL  // AVR clock frequency in Hz, used by util/delay.h
-#define SMART_BAUD 1000000UL  // Smartsensor baud rate
+#define SMART_BAUD 9600UL  // Smartsensor baud rate
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/atomic.h>
 #include <stdlib.h>
-#include <util/delay.h>
 
 // ****Forward declaration of functions****
 void uart_init (long BUADRATE);
@@ -31,7 +30,42 @@ void serialPrint(char *StringOfCharacters, size_t len);
 void LF();
 void CR();
 void init_sensor_io();
-void toggle_sensor_io();
+void receive_and_echo(); // TODO(tobinsarah): implement working r&e code in this fn
+
+// ****Global vars
+int volatile a;
+// receivedByte
+char volatile receivedByte;
+char rxBuffer[255];
+// rxBufferFlag = n: n = [0,255] means expecting to read nth byte of packet
+// n = 0: main is done, 0xFF: main is processing, 0xFE: rx is done
+int volatile rxBufferFlag;
+int volatile rxBufferIndex;
+int volatile packetType;
+int volatile dataLen;
+
+// Main
+int main() {
+  a = 0;
+  rxBufferFlag = 0;
+  rxBufferIndex = 0;
+  dataLen = 0;
+
+  init_sensor_io();
+
+  uart_init(SMART_BAUD);
+
+  while (1) {
+    if (rxBufferFlag == 0xFE) {  // if done receiving
+      rxBufferFlag++;
+      if (packetType == 0xFE) {  // if pingpong type
+        serialPrint(rxBuffer, (size_t) (dataLen));
+      }
+      rxBufferFlag = 0;  // should be below if statemt
+    }
+  }
+  return 1;
+}
 
 
 // function to initialize UART
@@ -46,14 +80,6 @@ void uart_init (long Desired_Baudrate)
 
   // Set to double speed mode.
   UCSR0A = UCSR0A | (1 << U2X0);
-
-  //Set baud rate.
-  UBRR0H = (unsigned char)(UBBR>>8);
-  UBRR0L = (unsigned char)UBBR;
-  //Enable receiver and transmitter
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-  //Set frame format: 8data, 1 stop bit
-  UCSR0C = (1<<USBS0)|(1<<UCSZ00)|(1<<UCSZ01);   // 8bit data format
 }
 
 void USART_Transmit( unsigned char data ) // and toggle
@@ -91,37 +117,42 @@ void init_sensor_io() {
   // C1 sould be IN0
   PORTC = (0 << PC0) | (0 << PC1) | (0 << PC2);
   DDRC = (1 << DDRC0) | (1 << DDRC1) | (1 << DDRC2);
-}
-
-//toggles one (soon to be more?) sensor
-void toggle_sensor_io() {
-  int x = 1;
-  while (x) {
-    PORTB = PORTB ^ (1 << PB3);
-  }
-}
-
-
-// Main
-int main(){
-  // portA not used right now so this should do nothing
-  PORTA = (0 << PA0) | (0 << PA1) | (0 << PA2);
-  DDRA = (1 << DDRA0) | (1 << DDRA1) | (1 << DDRA2);
-
-  // B3 should be IN1
-  PORTB = (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);
-  DDRB = (1 << DDRB0) | (1 << DDRB1) | (1 << DDRB2) | (1 << DDRB3);
-
-  // C1 sould be IN0
-  PORTC = (0 << PC0) | (0 << PC1) | (0 << PC2);
-  DDRC = (1 << DDRC0) | (1 << DDRC1) | (1 << DDRC2);
 
   uart_init(SMART_BAUD);
+}
 
-  char data[] = {0, 4, 1, 2};
 
-  while(1) {
-    serialPrint(data, 4);
+// interrupt for recieve and echo one byte.
+ISR(USART0__RX_vect) {
+  // Fetch the received byte value into the variable "ByteReceived"
+  receivedByte = UDR0;
+  if (rxBufferFlag == 0xFE || rxBufferFlag == 0xFF) {
+    PORTC = PORTC ^ (1 << PC1); // toggle IN0
+    return;
   }
-  return 1;
+  if (receivedByte == 0x00) { // if see zerobyte (any time when receinving)
+      rxBufferFlag = 0x01;
+  } else if (rxBufferFlag == 0x01 ) { // looking for type
+    packetType = receivedByte;
+    rxBufferFlag++;
+  } else if (rxBufferFlag == 0x02) {  // looking for len
+    PORTB = PORTB ^ (1 << PB2); // toggle IN3
+    dataLen = receivedByte - 3;
+    if (dataLen < 0); // TODO(tobinsarah): deal with too short packets;
+    if (dataLen == 0) {
+      rxBufferFlag = 0xFE;
+    } else {
+      rxBufferIndex = 0;
+      rxBufferFlag++;
+    }
+  } else if (rxBufferIndex < dataLen - 1) { // TODO(tobinsarah): deal with dataLen <= 3
+    PORTB = PORTB ^ (1 << PB1); // toggle IN2
+    rxBuffer[rxBufferIndex] = receivedByte;
+    rxBufferFlag++;
+    rxBufferIndex++;
+  } else {
+    PORTB = PORTB ^ (1 << PB3); // toggle IN1
+    rxBuffer[rxBufferIndex] = receivedByte;
+    rxBufferFlag = 0xFE;
+  }
 }
