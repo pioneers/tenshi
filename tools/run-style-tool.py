b3c03b2136c45ec9dd1e88d1f348943dfb1c79d3
Robert Ou <robertou@pioneers.berkeley.edu>
2014-06-25 23:02:53 -0700

    Licensing: Add Apache headers to all clearly-PiE-owned executable code
    This commit adds headers to all source code files that are clearly executable
    and not taken from elsewhere. This is the majority of the code in the project.
    In a future commit, other things such as e.g. data and images will be taken
    care of.

--- tools/run-style-tool.py
+++ tools/run-style-tool.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# Licensed to Pioneers in Engineering under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Pioneers in Engineering licenses
+# this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License
+
 from __future__ import print_function
 
 import os
@@ -79,7 +96,7 @@ def main():
     if styletool == "cpplint":
         exit_code = subprocess.call([
             './tools/cpplint.py',
-            '--filter=-legal/copyright,-readability/casting',
+            '--filter=-readability/casting',
             '--extensions=c,cpp,h,template'
             ] + source_files)
         sys.exit(exit_code)
