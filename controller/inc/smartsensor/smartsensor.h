272629c3106410152150b5838ec75efbfab43c9b
Casey Duckering <cduckering@gmail.com>
2014-06-26 03:17:15 -0700

    New smart sensor related constants

--- controller/inc/smartsensor/smartsensor.h
+++ controller/inc/smartsensor/smartsensor.h
@@ -1,11 +1,14 @@
 #ifndef INC_SMARTSENSOR_SMARTSENSOR_H_
 #define INC_SMARTSENSOR_SMARTSENSOR_H_
 
-#include "inc/smartsensor/ssutil.h"
-
 #include "inc/FreeRTOS.h"
-
+#include "inc/smartsensor/ssutil.h"
 #include "inc/uart_serial_driver.h"
+
+#define SS_NUM_SAMPLES 8  // 3 bits of resolution
+#define SS_NUM_FRAMES 6
+#define SS_FIRST_FRAME 1  // 1 indexed frame numbers
+
 
 void smartsensor_init();
 
