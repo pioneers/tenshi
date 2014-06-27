87e85b5a23fa2d8bcf465a9413da17ae6bb0c48d
Sumita Ghosh <sumitag@berkeley.edu>
2014-06-26 20:48:55 -0700

    Analog In

---
+++ smartsensor_fw/inc/analog_in.h
@@ -0,0 +1,12 @@
+
+#ifndef INC_ANALOG_IN_H_
+#define INC_ANALOG_IN_H_
+
+#include "inc/common.h"
+
+// Public functions called from main.c
+void initAnalogIn();
+void activeAnalogInRec(uint8_t *data, uint8_t len, uint8_t inband);
+void activeAnalogInSend(uint8_t *outData, uint8_t *outLen);
+
+#endif  // INC_ANALOG_IN_H_
