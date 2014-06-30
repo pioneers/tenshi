6c0dab125d08f95c621cb7cfd0a2b463bf9c472a
Amit <amitvakula@berkeley.edu>
2014-06-29 17:06:27 -0700

    added Java implementation of enum algorithm

---
+++ controller/src/smartsensor/Sandbox.java
@@ -0,0 +1,23 @@
+
+public class Sandbox 
+{
+	public static void main(String[] args)
+	{
+		int one = 5;
+		one = 5 << 1;
+		System.out.println(one);
+	}
+	
+	public static boolean inID(int ID, int id_num, int pos)
+	{
+		int start = 1; 
+		while (start <= pos)
+		{
+			
+		}
+		return true;
+	}
+	
+
+ 
+}
