b3702691e1cc55507f2f7fab65d64d15d8f9f2df
Nikita Kitaev <nikitakitaev@pioneers.berkeley.edu>
2014-06-29 19:21:01 -0700

    Switch to persistent divs for main ui
    The angular-based framework will preload all of the elements into divs
    and toggle their layout and visibility to achieve module switching. The
    broken layout of this version will be fixed once angular has been added.
    
    Disabling resource-intensive features (such as the simulator's physics
    engine and rendering) when not visible will be handled within the
    individual modules.
    
    The robot maker is temporarily disabled because the simulator and the
    maker use the same variable to store the window object.

--- angel-player/src/chrome/content/main-ui/ui.js
+++ angel-player/src/chrome/content/main-ui/ui.js
@@ -9,109 +9,65 @@ let currentSelectedTab = -1;
 
 const { isDebugEnabled } = require('tenshi/common/tenshi_prefs');
 
-// Different ways to arrange the divs to suit different environments. Contains
-// relative flexbox sizes for each element.
-const LAYOUTS = {
-    "center-with-all-sidebars": {
-        top: "1",
-        centerGroup: "8",
-        bottom: "1",
-        left: "1",
-        centerPane: "8",
-        right: "1",
-    },
-    "center-only": {
-        top: "0",
-        centerGroup: "1",
-        bottom: "0",
-        left: "0",
-        centerPane: "1",
-        right: "0",
-    },
-    "split-view-header-footer": {
-        top: "1",
-        centerGroup: "7",
-        bottom: "2",
-        left: "1",
-        centerPane: "0",
-        right: "1",
-    }
-};
-
 // Different "environments" such as coding, driving, home page, etc.
 var ENVIRONMENTS = [
     {
         text: "Home",
-        image: 'chrome://angel-player/content/main-ui/assets/home.png',
         activate: function(prev) {
-            switchLayout("center-only");
-            loadPageIntoDiv("centerDiv", "../welcome/welcome.html");
+            $("#welcome-div").show();
         },
         deactivate: function(next) {
-            $("#centerDiv").empty();
+            $("#welcome-div").hide();
         },
     }, {
         text: "Code",
         image: 'chrome://angel-player/content/main-ui/assets/code.png',
         activate: function(prev) {
-            switchLayout("split-view-header-footer");
-            loadPageIntoDiv("topDiv", "../controls/main.html");
-            loadPageIntoDiv("leftDiv", "../texteditor/editor.html");
-            // TODO(rqou): API help page
-            loadPageIntoDiv("bottomDiv", "../console/console.html");
+            $("#controls-div").show();
+            $("#texteditor-div").show();
+            $("#console-div").show();
         },
         deactivate: function(next) {
-            $("#topDiv").empty();
-            $("#leftDiv").empty();
-            $("#bottomDiv").empty();
+            $("#controls-div").hide();
+            $("#texteditor-div").hide();
+            $("#console-div").hide();
         },
     }, {
         text: "Block Editor",
         image: 'chrome://angel-player/content/main-ui/assets/blockeditor.png',
         activate: function(prev) {
-            switchLayout("split-view-header-footer");
-            loadPageIntoDiv("topDiv", "../controls/main.html");
-            loadPageIntoDiv("leftDiv", "../blockeditor/blockeditor.html");
-            // TODO(rqou): API help page
-            loadPageIntoDiv("bottomDiv", "../console/console.html");
+            $("#controls-div").show();
+            $("#blockeditor-div").show();
+            $("#console-div").show();
         },
         deactivate: function(next) {
-            $("#topDiv").empty();
-            $("#leftDiv").empty();
-            $("#bottomDiv").empty();
+            $("#controls-div").hide();
+            $("#blockeditor-div").hide();
+            $("#console-div").hide();
         },
     }, {
         text: "Simulator",
         image: 'chrome://angel-player/content/main-ui/assets/simulator.png',
         activate: function(prev) {
-            // TODO(rqou): Don't unload a page just to reload it again!
-            // TODO(rqou): A way to map names back to indices.
-            switchLayout("split-view-header-footer");
-            loadPageIntoDiv("topDiv", "../controls/main.html");
-            if (prev == 2) {
-                // Block editor was previous
-                loadPageIntoDiv("leftDiv", "../blockeditor/blockeditor.html");
-            } else {
-                loadPageIntoDiv("leftDiv", "../texteditor/editor.html");
-            }
-            loadPageIntoDiv("rightDiv", "../simulator/main.html");
-            loadPageIntoDiv("bottomDiv", "../console/console.html");
+            $("#controls-div").show();
+            $("#texteditor-div").show();
+            $("#simulator-div").show();
+            $("#console-div").show();
         },
         deactivate: function(next) {
-            $("#topDiv").empty();
-            $("#leftDiv").empty();
-            $("#rightDiv").empty();
-            $("#bottomDiv").empty();
+            $("#controls-div").hide();
+            $("#texteditor-div").hide();
+            $("#simulator-div").hide();
+            $("#console-div").hide();
         },
     }, {
         text: "Robot Builder",
         image: 'chrome://angel-player/content/main-ui/assets/builder.png',
         activate: function(prev) {
-            switchLayout("center-only");
-            loadPageIntoDiv("centerDiv", "../simulator/maker.html");
+            $("#maker-div").show();
         },
         deactivate: function(next) {
-            $("#centerDiv").empty();
+            $("#maker-div").hide();
         },
     },
 ];
@@ -123,31 +79,13 @@ var ENVIRONMENTS = [
 if (isDebugEnabled()) {
   ENVIRONMENTS.push({
         text: "Stargate Network",
-        image: 'chrome://angel-player/content/main-ui/assets/stargate.png',
         activate: function(prev) {
-            switchLayout("center-only");
-            loadPageIntoDiv("centerDiv", "../debug-main.html");
+            $("#stargate-div").show();
         },
         deactivate: function(next) {
-            $("#centerDiv").empty();
+            $("#stargate-div").hide();
         },
   });
-}
-
-function loadPageIntoDiv(div, page) {
-    // TODO(rqou): Refactor paths
-    $("<iframe class=\"absolutelyNoSpace\" " +
-        "src=\"" + page + "\"></iframe>").appendTo($("#" + div));
-}
-
-function switchLayout(newLayout) {
-    let layout = LAYOUTS[newLayout];
-    $("#topDiv").css('flex', layout.top);
-    $("#mainCenter").css('flex', layout.centerGroup);
-    $("#bottomDiv").css('flex', layout.bottom);
-    $("#leftDiv").css('flex', layout.left);
-    $("#centerDiv").css('flex', layout.centerPane);
-    $("#rightDiv").css('flex', layout.right);
 }
 
 function newTabSelected() {
