cab1381c0aafb21ea25028cb123f1dfae46e9699
Casey Duckering <cduckering@gmail.com>
2014-07-05 04:43:27 -0700

    Fix grizzly deploy and flash scripts to work in the tenshi build system

--- grizzly_firmware/reflash.sh
+++ grizzly_firmware/reflash.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 echo "Connect Grizzly to test jig and AVR programmer."
-echo "Choose yes if prompted."
 echo
 
-sudo avrdude -c usbtiny -p atmega32u4 -U lfuse:w:0xff:m -U hfuse:w:0xD9:m -U efuse:w:0xFB:m
 sudo avrdude -c usbtiny -p atmega32u4 -U flash:w:../build/artifacts/grizzly_firmware/opt/grizzly_firmware.hex:i
 
