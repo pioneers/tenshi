27dded1dea653f24674eddfadea360b0a20da2b8
Robert Ou <robertou@pioneers.berkeley.edu>
2014-07-10 23:02:55 -0700

    Fix missing deps in install_build_deps.sh

--- install_build_deps.sh
+++ install_build_deps.sh
@@ -3,6 +3,14 @@
 # This script will install packages required to build tenshi on Ubuntu
 #
 
+if [[ $EUID -ne 0 ]]
+then
+    echo "This script must be run with root privileges"
+    echo "Running script with 'sudo'"
+    sudo $0 $*
+    exit 0
+fi
+
 ## Ubuntu version checking
 
 [[ -f /etc/lsb-release ]] || {
@@ -51,23 +59,23 @@ pushd "$TMPDIR"
 trap 'popd; rm -rf $TMPDIR; exit 15' 1 2 3 15
 
 ## Install misc dependencies
-sudo apt-get install xvfb openjdk-7-jre-headless coreutils build-essential
+apt-get install xvfb openjdk-7-jre-headless coreutils build-essential software-properties-common wget zip
 
 ## Install Python and related
 which pip >/dev/null || {
-	sudo apt-get install python-pip
+	apt-get install python-pip python-dev libyaml-dev
 }
 
-sudo pip install pyyaml pep8
+pip install pyyaml pep8
 
 ## Install nodejs and related
 which node >/dev/null || {
-	sudo add-apt-repository ppa:chris-lea/node.js
-	sudo apt-get update
-	sudo apt-get install nodejs
+	add-apt-repository ppa:chris-lea/node.js
+	apt-get update
+	apt-get install nodejs
 }
 
-sudo npm install jshint csslint -g
+npm install jshint csslint -g
 
 
 ## Install atmel toolchain
@@ -75,24 +83,24 @@ sudo npm install jshint csslint -g
 ### Dependency for the toolchain
 dpkg -s libmpc2 >/dev/null || {
 	wget http://launchpadlibrarian.net/118496223/libmpc2_0.9-4build1_amd64.deb
-	sudo dpkg -i libmpc2_0.9-4build1_amd64.deb
+	dpkg -i libmpc2_0.9-4build1_amd64.deb
 }
 
 dpkg -s atmel-toolchain-binutils-avr atmel-toolchain-gcc-avr atmel-toolchain-avr-libc > /dev/null || {
 	### Set up PPA
-	sudo add-apt-repository ppa:nonolith/avr-toolchain
+	add-apt-repository ppa:nonolith/avr-toolchain
 
 	if [ -f /etc/apt/sources.list.d/nonolith-avr-toolchain-trusty.list ]; then
 		# The PPA is not currently up-to-date for Ubuntu 14.04, so use the
 		# 13.10 sources instead
 		sed "s/trusty/saucy/g" /etc/apt/sources.list.d/nonolith-avr-toolchain-trusty.list > nonolith-avr-toolchain-trusty.list
-		sudo mv nonolith-avr-toolchain-trusty.list /etc/apt/sources.list.d
+		mv nonolith-avr-toolchain-trusty.list /etc/apt/sources.list.d
 	fi
 
-	sudo apt-get update
+	apt-get update
 
 	### Toolchain installation
-	sudo apt-get install atmel-toolchain-binutils-avr atmel-toolchain-gcc-avr atmel-toolchain-avr-libc
+	apt-get install atmel-toolchain-binutils-avr atmel-toolchain-gcc-avr atmel-toolchain-avr-libc
 }
 
 
@@ -106,12 +114,22 @@ which eagle && EAGLE_INSTALLED=1
 ### If there is no existing installation, install eagle
 
 if [[ "$EAGLE_INSTALLED" = 0 ]]; then
-	sudo apt-get install libxrender1:i386 libxrandr2:i386 libxcursor1:i386 libfreetype6:i386 libfontconfig1:i386 libxi6:i386 libpng12-0:i386 libstdc++6:i386 libjpeg62:i386 libssl1.0.0:i386
+	set +e
+	dpkg --print-foreign-architectures | grep i386
+	is_i386_arch_enabled=$?
+	set -e
+	if [[ $is_i386_arch_enabled != 0 ]]; then
+		dpkg --add-architecture i386
+	fi
+	apt-get update
+	apt-get install libxrender1:i386 libxrandr2:i386 libxcursor1:i386 libfreetype6:i386 libfontconfig1:i386 libxi6:i386 libpng12-0:i386 libstdc++6:i386 libjpeg62:i386 libssl1.0.0:i386
 	wget ftp://ftp.cadsoft.de/eagle/program/6.5/eagle-lin-6.5.0.run
 	chmod +x eagle-lin-6.5.0.run
-	sudo ./eagle-lin-6.5.0.run
+	./eagle-lin-6.5.0.run
 fi
 
+## Dependencies for running xulrunner
+apt-get install libxrender1 libasound2 libgtk2.0-0
 
 ## Cleanup
 popd
