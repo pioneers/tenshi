983f7367bf69b3dee9a18db9c3909ec02c21ba7d
Casey Duckering <cduckering@gmail.com>
2014-07-12 01:39:10 -0700

    Static student Lua code for driving robot with one analog sensor and two motors

--- controller/inc/lua_interface.h
+++ controller/inc/lua_interface.h
@@ -7,16 +7,36 @@
 
 
 #define TEST_STATIC_LUA \
-  /*"while get_digital(0)==0 then"\
-  "end"\
-  "a=1"*/\
-  "while 1 do\n"\
-  "   set_led(get_digital(0))"\
-  /*"   a = a+1"*/\
+  "while get_digital(0)==0 do\n" /* Wait until sensors enumerate */\
+  "end\n"\
+  \
+  "started = false\n"\
+  "dead_zone = 20\n"\
+  "range = 1024\n"\
+  "midpoint = range/2\n"\
+  "max_motor = 100\n"\
+  \
+  "while 1 do\n"                 /* Control loop */\
+  "   sensor = get_analog(0)\n"\
+  "   set_led(sensor)\n"\
   /*"   set_led((get_digital(0)%2)*2+(get_digital(1)%2)*4)\n"*/\
   /*"   set_digital(0,get_digital(1)*2)\n"*/\
-  "   set_motor(1,get_digital(0)<10 and 0 or 100)\n"\
-  "end"  /* NOLINT(*) */
+  "   if not started and math.abs(sensor-midpoint)<=dead_zone then\n"\
+  "      started = true\n"\
+  "   end\n"\
+  "   speed = 0\n"\
+  \
+  "   if started then\n"\
+  "     speed = (sensor-midpoint)*max_motor/(range/2)\n"\
+  "     \n"\
+  "     \n"\
+  "     \n"\
+  "     \n"\
+  "   end\n"\
+  \
+  "   set_motor(1,speed)\n"\
+  "   set_motor(2,speed)\n"\
+  "end\n"  /* NOLINT(*) */
 
 
 
