c0c87517b2515e82a381b9e1677530aaa7fadcea
Tobin Holcomb <Tobinholcomb@pioneers.berkeley.edu>
2014-07-20 23:04:46 -0700

    Smart sensor firmware: beginning of descriptor packet implementation (0xD1)

--- smartsensor_fw/inc/config.h
+++ smartsensor_fw/inc/config.h
@@ -24,7 +24,14 @@
 #define SENSOR_TYPE_FLAG 0x82
 // ...
 
+
+// Individual Sensor data.
+// todo(tobinsarah): DO THIS RIGHT!
+#define HUMAN_READABLE_DESCRIPTION  'h', 'e', 'l', 'l', 'o'
 #define SENSOR_TYPE SENSOR_TYPE_ANALOG_IN
+
+#define DESCRIPTOR 0xFF, 5, HUMAN_READABLE_DESCRIPTION, 0xFF, 0xFF, 1, \
+                   SENSOR_TYPE,  0xFF
 
 
 #endif  // INC_CONFIG_H_
