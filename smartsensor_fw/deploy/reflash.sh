08d831c656e822d420da3647c7136f1544535066
Casey Duckering <cduckering@gmail.com>
2014-07-22 05:55:23 -0700

    New smart sensor deploy script that takes the sensor type as an argument and inserts an appropriate ID and descriptor into the hex file before flashing
    Only digital-in sensors are supported so far

--- smartsensor_fw/deploy/reflash.sh
+++ smartsensor_fw/deploy/reflash.sh
@@ -20,7 +20,7 @@
 SCRIPT_PATH=$(dirname `which $0`)
 cd $SCRIPT_PATH
 
-if diff /etc/avrdude.conf avrdude.conf >/dev/null ; then
+if diff /etc/avrdude.conf ../../tools/avrdude.conf >/dev/null ; then
   echo
 else
   echo "$(tput setaf 1)Warning: Your /etc/avrdude.conf file may not be correct.
@@ -29,5 +29,12 @@ or press [Enter] to continue anyways.$(tput sgr 0)"
   read
 fi
 
+set -e #exit on a non-zero exit code
+
+python generate.py -d "$@"
+
 set -x #echo on
-sudo avrdude -c usbtiny -p attiny1634 -U flash:w:../build/artifacts/smartsensor_fw/opt/smartsensor_fw.hex:i
+
+python generate.py "$@" |
+  sudo avrdude -c usbtiny -p attiny1634 -U flash:w:-:i
+
