1039b129bdeeefd1ff1f92be6b2acfa6de96509f
Casey Duckering <cduckering@gmail.com>
2014-07-23 01:24:01 -0700

    Fix grizzly deploy script to use the new hex file modification script

--- grizzly_firmware/deploy.sh
+++ grizzly_firmware/deploy.sh
@@ -22,5 +22,6 @@ echo "Choose yes if prompted."
 echo
 
 sudo avrdude -c usbtiny -p atmega32u4 -U lfuse:w:0xff:m -U hfuse:w:0xD9:m -U efuse:w:0xFB:m
-sudo avrdude -c usbtiny -p atmega32u4 -U flash:w:../build/artifacts/grizzly_firmware/opt/grizzly_firmware.hex:i
+python ../smartsensor_fw/deploy/generate.py -i ../build/artifacts/grizzly_firmware/opt/grizzly_firmware.hex grizzly |
+  sudo avrdude -c usbtiny -p atmega32u4 -U flash:w:-:i
 
