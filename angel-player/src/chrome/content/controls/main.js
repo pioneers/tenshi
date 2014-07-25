ac1f6bb6d8605dd38d334cd94fa0db0c52db6cf9
Robert Ou <robertou@pioneers.berkeley.edu>
2014-07-24 21:42:48 -0700

    Refactored out Lua compile and properly print out errors

--- angel-player/src/chrome/content/controls/main.js
+++ angel-player/src/chrome/content/controls/main.js
@@ -7,7 +7,8 @@ let {$} = window;
 let naive_packetizer = require('tenshi/common/naive_packetizer'),
     texteditor = require('tenshi/texteditor/editor'),
     studentconsole = require('tenshi/console/console'),
-    pieles = require('tenshi/pieles/pieles');
+    pieles = require('tenshi/pieles/pieles'),
+    lua = require('tenshi/lang-support/lua');
 
 let current_vm = null,
     vm_generator = function () {return null;};
@@ -81,30 +82,15 @@ function compile_ngl() {
 function compile_lua() {
     var text = texteditor.get_text();
 
-    let lua = require('tenshi/vendor-js/lua');
-    let lua_state = lua.ccall('luaL_newstate', 'number');
-    if (lua.ccall('luaL_loadstring', 'number',
-        ['number', 'string'], [lua_state, text]) !== 0) {
-        // TODO(rqou): Report the actual error
-        throw "Didn't load properly!";
+    let ret = lua.compile_lua(text);
+    if (!ret.success) {
+        studentconsole.report_error(ret.error);
+        return;
     }
 
-    let lua_bytecode = new Uint8Array(0);
-    function lua_dump_callback(L, p, sz, ud) {
-        let lua_bytecode_new = new Uint8Array(lua_bytecode.length + sz);
-        lua_bytecode_new.set(lua_bytecode);
-        lua_bytecode_new.set(
-            lua.HEAPU8.subarray(p, p + sz), lua_bytecode.length);
-        lua_bytecode = lua_bytecode_new;
+    studentconsole.report_message("Compiled successfully!");
+    for (let i = 0; i < ret.bytecode.length; i++) {
+        console.log(ret.bytecode[i]);
     }
-    let lua_dump_callback_ptr = lua.Runtime.addFunction(lua_dump_callback);
-    if (lua.ccall('lua_dump', 'number',
-        ['number', 'number', 'number'],
-        [lua_state, lua_dump_callback_ptr, 0]) !== 0) {
-        throw "Didn't dump properly!";
-    }
-    lua.ccall('lua_close', null, ['number'], [lua_state]);
-    lua.Runtime.removeFunction(lua_dump_callback_ptr);
-
-    naive_packetizer.sendPacketizedData(lua_bytecode);
+    naive_packetizer.sendPacketizedData(ret.bytecode);
 }
