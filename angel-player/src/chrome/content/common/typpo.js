8106f03ccb12976a1bc8da1ce79bb846984413bb
Kyle Zentner <zentner.kyle@gmail.com>
2014-07-31 00:55:04 -0700

    Put a configured typpo factory in common/typpo.js

---
+++ angel-player/src/chrome/content/common/typpo.js
@@ -0,0 +1,21 @@
+/* jshint globalstrict: true */
+"use strict";
+
+const typpo_module = require('tenshi/common/factory');
+const url = require('sdk/url');
+
+const PIEMOS_FRAMING_YAML_FILE =
+    'chrome://angel-player/content/common_defs/legacy_piemos_framing.yaml';
+
+const XBEE_FRAMING_YAML_FILE =
+    'chrome://angel-player/content/common_defs/xbee_typpo.yaml';
+
+
+// Init Typpo
+let typpo = typpo_module.make();
+typpo.set_target_type('ARM');
+typpo.load_type_file(url.toFilename(PIEMOS_FRAMING_YAML_FILE), false);
+typpo.load_type_file(url.toFilename(XBEE_FRAMING_YAML_FILE), false);
+
+
+module.exports = typpo;
