e36009b2591574bf423517a591c395ea87fbafd4
Kyle Zentner <zentner.kyle@gmail.com>
2014-08-05 21:16:57 -0700

    Begin work on config port.

--- angel-player/src/chrome/content/common/typpo.js
+++ angel-player/src/chrome/content/common/typpo.js
@@ -10,12 +10,16 @@ const PIEMOS_FRAMING_YAML_FILE =
 const XBEE_FRAMING_YAML_FILE =
     'chrome://angel-player/content/common_defs/xbee_typpo.yaml';
 
+const CONFIG_YAML_FILE =
+    'chrome://angel-player/content/common_defs/config.yaml';
+
 
 // Init Typpo
 let typpo = typpo_module.make();
 typpo.set_target_type('ARM');
 typpo.load_type_file(url.toFilename(PIEMOS_FRAMING_YAML_FILE), false);
 typpo.load_type_file(url.toFilename(XBEE_FRAMING_YAML_FILE), false);
+typpo.load_type_file(url.toFilename(CONFIG_YAML_FILE), false);
 
 
 module.exports = typpo;
